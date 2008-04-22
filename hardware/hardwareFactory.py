512073031286347be2800f5bc3da087bf673cc44
fma <fma@1bb8f0fc-47fd-4586-b4e1-c5ba9e517023>
2008-04-22 10:06:03 +0000

    First commit

---
+++ hardware/hardwareFactory.py
@@ -0,0 +1,40 @@
+# -*- coding: iso-8859-1 -*-
+
+""" Panohead remote control.
+
+Hardware classes.
+
+Implements class:
+
+- HardwareFactory
+
+@author: Frederic Mantegazza
+@copyright: 2007-2008
+@license: CeCILL
+@todo:
+"""
+
+__revision__ = "$Id$"
+
+from common.exception import HardwareError
+from head import Head, HeadSimulation
+
+
+class HardwareFactory(object):
+    """ Class for creating hardware.
+    """
+    def create(self, type_):
+        """ create a hardware object.
+        
+        @param type_: type of hardware object
+        @type type_: str
+        
+        @raise HardwareError: unknown type
+        """
+        if type_ == "head":
+            return Head()
+        elif type_ == "headSimulation":
+            return HeadSimulation()
+
+        else:
+            raise HardwareError("Unknow '%s' driver type" % type_)
