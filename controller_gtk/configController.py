9be7d751599c453a166b732955804add11202cc0
fma <fma@1bb8f0fc-47fd-4586-b4e1-c5ba9e517023>
2008-04-29 19:51:10 +0000

    First commit

---
+++ controller_gtk/configController.py
@@ -0,0 +1,106 @@
+# -*- coding: iso-8859-1 -*-
+
+""" Panohead remote control.
+
+Controller.
+
+Implements class:
+
+- ConfigController
+
+@author: Frederic Mantegazza
+@copyright: 2007
+@license: CeCILL
+@todo:
+"""
+
+__revision__ = "$Id$"
+
+import copy
+
+from common import config
+from common.loggingServices import Logger
+from controller.abstractController import AbstractController
+
+
+class ConfigController(AbstractController):
+    """ Configuration controller object.
+    """
+    def __init__(self, parent, model, view):
+        """ Init the object.
+
+        @param parent: parent controller
+        @type parent: {Controller}
+
+        @param model: model to use
+        @type mode: {Shooting}
+
+        @param view: associated view
+        @type view: {ConfigDialog}
+        """
+        self.__parent = parent
+        self.__model = model
+        self.__view = view
+
+        # Connect signal/slots
+        dic = {"on_okButton_clicked": self.__onOkButtonClicked,
+               "on_cancelButton_clicked": self.__onCancelButtonClicked,
+               "on_defaultButton_clicked": self.__onDefaultButtonClicked
+           }
+        self.__view.wTree.signal_autoconnect(dic)
+        
+        # Fill widgets
+        self.refreshView()
+
+    def __onOkButtonClicked(self, widget):
+        """ Ok button has been clicked.
+        
+        Save back values to model.
+        """
+        Logger().trace("ConfigController.__onOkButtonClicked()")
+        self.__model.camera.timeValue = self.__view.timeValueSpinbutton.get_value()
+        self.__model.camera.nbPicts = self.__view.nbPictsSpinbutton.get_value()
+        self.__model.delay = self.__view.delaySpinbutton.get_value()
+        self.__model.camera.sensorCoef = self.__view.sensorCoefSpinbutton.get_value()
+        self.__model.camera.sensorRatio = config.SENSOR_RATIOS_INDEX[self.__view.sensorRatioCombobox.get_active()]
+        self.__model.overlap = self.__view.overlapSpinbutton.get_value() / 100.
+        self.__model.camera.lens.focal = self.__view.focalSpinbutton.get_value()
+        self.__model.camera.lens.fisheye = self.__view.fisheyeCheckbutton.get_active()
+        self.__model.cameraOrientation = config.CAMERA_ORIENTATION_INDEX[self.__view.cameraOrientationCombobox.get_active()]
+        self.__model.manualShoot = self.__view.manualShootCheckbutton.get_active()
+
+    def __onCancelButtonClicked(self, widget):
+        """ Cancel button has been clicked.
+        
+        Close the pref. dialog.
+        """
+        Logger().trace("ConfigController.__onCancelButtonClicked()")
+
+    def __onDefaultButtonClicked(self, widget):
+        """ Default button has been clicked.
+        
+        Load default config values.
+        """
+        Logger().trace("ConfigController.__onDefaultButtonClicked()")
+        defaultValues = copy.deepcopy(config.DEFAULT_PREFS)
+        defaultValues['shooting']['overlap'] *= 100
+        self.__view.fillWidgets(defaultValues)
+        
+    def refreshView(self):
+        values = {'shooting': {'overlap': int(100 * self.__model.overlap),
+                               'manualShoot': self.__model.manualShoot,
+                               'cameraOrientation': self.__model.cameraOrientation,
+                               'delay': self.__model.delay
+                               },
+                  'mosaic': {'template': self.__model.mosaic.template
+                           },
+                  'camera': {'sensorCoef': self.__model.camera.sensorCoef,
+                             'sensorRatio': self.__model.camera.sensorRatio,
+                             'timeValue': self.__model.camera.timeValue,
+                             'nbPicts' : self.__model.camera.nbPicts,
+                         },
+                  'lens': {'focal': self.__model.camera.lens.focal,
+                           'fisheye': self.__model.camera.lens.fisheye
+                       }
+                }
+        self.__view.fillWidgets(values)
---
+++ view_gtk/configDialog.py
@@ -0,0 +1,71 @@
+# -*- coding: iso-8859-1 -*-
+
+""" Panohead remote control.
+
+View.
+
+Implements class:
+
+- ConfigDialog
+
+@author: Frederic Mantegazza
+@copyright: 2007
+@license: CeCILL
+@todo:
+"""
+
+__revision__ = "$Id$"
+
+import os.path
+
+#import pygtk
+#pygtk.require("2.0")
+import gtk.glade
+
+path = os.path.dirname(__file__)
+
+from common import config
+from common.loggingServices import Logger
+
+
+class ConfigDialog(object):
+    """ Preference dialog.
+    """
+    def __init__(self):
+        """ Init the object.
+        """
+        # Set the Glade file
+        gladeFile = os.path.join(path, "configDialog.glade")
+        self.wTree = gtk.glade.XML(gladeFile) 
+        
+        # Retreive usefull widgets
+        self._retreiveWidgets()
+ 
+    def _retreiveWidgets(self):
+        """ Get widgets from widget tree.
+        """
+        self.configDialog = self.wTree.get_widget("configDialog")
+        self.timeValueSpinbutton = self.wTree.get_widget("timeValueSpinbutton")
+        self.nbPictsSpinbutton = self.wTree.get_widget("nbPictsSpinbutton")
+        self.delaySpinbutton = self.wTree.get_widget("delaySpinbutton")
+        self.sensorCoefSpinbutton = self.wTree.get_widget("sensorCoefSpinbutton")
+        self.sensorRatioCombobox = self.wTree.get_widget("sensorRatioCombobox")
+        self.overlapSpinbutton = self.wTree.get_widget("overlapSpinbutton")
+        self.focalSpinbutton = self.wTree.get_widget("focalSpinbutton")
+        self.fisheyeCheckbutton = self.wTree.get_widget("fisheyeCheckbutton")
+        self.cameraOrientationCombobox = self.wTree.get_widget("cameraOrientationCombobox")
+        self.manualShootCheckbutton = self.wTree.get_widget("manualShootCheckbutton")
+
+    def fillWidgets(self, values):
+        """ Fill widgets with values.
+        """
+        self.timeValueSpinbutton.set_value(values['camera']['timeValue'])
+        self.nbPictsSpinbutton.set_value(values['camera']['nbPicts'])
+        self.delaySpinbutton.set_value(values['shooting']['delay'])
+        self.sensorCoefSpinbutton.set_value(values['camera']['sensorCoef'])
+        self.sensorRatioCombobox.set_active(config.SENSOR_RATIOS_INDEX[values['camera']['sensorRatio']])
+        self.overlapSpinbutton.set_value(values['shooting']['overlap'])
+        self.focalSpinbutton.set_value(values['lens']['focal'])
+        self.fisheyeCheckbutton.set_active(values['lens']['fisheye'])
+        self.cameraOrientationCombobox.set_active(config.CAMERA_ORIENTATION_INDEX[values['shooting']['cameraOrientation']])
+        self.manualShootCheckbutton.set_active(values['shooting']['manualShoot'])
