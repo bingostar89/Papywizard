ea66bea368f246064735a8fe5e27cdfdcb3f0825
fma <fma@1bb8f0fc-47fd-4586-b4e1-c5ba9e517023>
2008-05-02 13:22:41 +0000

    Cleanup code

--- controller_gtk/shootController.py
+++ controller_gtk/shootController.py
@@ -56,13 +56,10 @@ class ShootController(AbstractController):
                "on_suspendResumeButton_clicked": self.__onSuspendResumeButtonClicked,
                "on_stopButton_clicked": self.__onStopButtonClicked,
                "on_doneButton_clicked": self.__onDoneButtonClicked,
-               #"on_shootDialog_key_press_event": self.__onKeyPressed,
-               #"on_shootDialog_key_release_event": self.__onKeyReleased,
            }
         self.__view.wTree.signal_autoconnect(dic)
         self.__view.shootDialog.connect("key-press-event", self.__onKeyPressed)
         self.__view.shootDialog.connect("key-release-event", self.__onKeyReleased)
-        #self.__suspendResumeHandler = self.__view.suspendResumeButton.connect('clicked', self.__onSuspendButtonClicked)
         
         self.__keyPressedDict = {'Left': False,
                                  'Right': False,
@@ -84,8 +81,6 @@ class ShootController(AbstractController):
         # Nokia plateform stuff
         try:
             import hildon
-            #self.__view.shootDialog.connect("key-press-event", self.__onKeyPressed)
-            #self.__view.shootDialog.connect("key-release-event", self.__onKeyReleased)
             self.__key['Home'] = gtk.keysyms.F8
             self.__key['End'] = gtk.keysyms.F7
         except ImportError:
@@ -97,6 +92,7 @@ class ShootController(AbstractController):
         # Connect Spy
         Spy().newPosSignal.connect(self.__refreshPos)
 
+    # Callbacks
     def __onKeyPressed(self, widget, event, *args):
                 
         # 'Right' key
@@ -175,7 +171,8 @@ class ShootController(AbstractController):
             # Pressing 'Escape' while not shooting exit shoot dialog
             if not self.__model.isShooting():
                 Logger().info("shootController.__onKeyPressed(): close shooting dialog")
-                self.__view.doneButton.clicked() # Find a better way (response does not work!!!)
+                self.__view.shootDialog.response(0)
+                #self.__view.doneButton.clicked() # Find a better way (response does not work!!!)
                 
             # Pressing 'Escape' while shooting stops shooting
             else:
@@ -238,6 +235,37 @@ class ShootController(AbstractController):
         else:
             Logger().warning("MainController.__onKeyReleased(): unbind '%s' key" % event.keyval)
 
+    def __onStartButtonClicked(self, widget):
+        """ Start button has been clicked.
+        
+        The model's start() method is called in a thread
+        """
+        Logger().trace("ShootController.__startButtonClicked()")
+        self.__startShooting()
+    
+    def __onSuspendResumeButtonClicked(self, widget):
+        """ SuspendResume button has been clicked.
+        """
+        Logger().trace("ShootController.__suspendResumeButtonClicked()")
+        if self.__model.isShooting(): # Should always be true here, but...
+            if not self.__model.isSuspended():
+                self.__suspendShooting()
+            else:
+                self.__resumeShooting()
+        
+    def __onStopButtonClicked(self, widget):
+        """ Stop button has been clicked.
+        """
+        Logger().trace("ShootController.__stopButtonClicked()")
+        self.__stopShooting()
+        
+    def __onDoneButtonClicked(self, widget):
+        """ Done button has been clicked.
+        """
+        Logger().trace("ShootController.__onDoneButtonClicked()")
+        self.__view.shootDialog.response(0)
+
+    # Real work
     def __startShooting(self):
         def checkEnd():
             """ Check end of shooting.
@@ -251,8 +279,6 @@ class ShootController(AbstractController):
                 Logger().debug("checkEnd(): model not shooting anymore")
                 self.__view.startButton.set_sensitive(True)
                 self.__view.suspendResumeButton.set_label("Suspend")
-                #self.__view.suspendResumeButton.disconnect(self.__suspendResumeHandler)
-                #self.__suspendResumeHandler = self.__view.suspendResumeButton.connect('clicked', self.__onSuspendButtonClicked)
                 self.__view.suspendResumeButton.set_sensitive(False)
                 self.__view.stopButton.set_sensitive(False)
                 self.__view.doneButton.set_sensitive(True)
@@ -281,47 +307,13 @@ class ShootController(AbstractController):
         gobject.timeout_add(200, checkEnd)
         #checkEnd()
 
-    def __onStartButtonClicked(self, widget):
-        """ Start button has been clicked.
-        
-        The model's start() method is called in a thread
-        """
-        Logger().trace("ShootController.__startButtonClicked()")
-        self.__startShooting()
-    
     def __suspendShooting(self):
         self.__model.suspend()
         self.__view.suspendResumeButton.set_label("Resume")
-        #self.__view.suspendResumeButton.disconnect(self.__suspendResumeHandler)
-        #self.__suspendResumeHandler = self.__view.suspendResumeButton.connect('clicked', self.__onResumeButtonClicked)
-    
-    #def __onSuspendButtonClicked(self, widget):
-        #""" Suspend button has been clicked.
-        #"""
-        #Logger().trace("ShootController.__suspendButtonClicked()")
-        #self.__suspendShooting()
     
     def __resumeShooting(self):
         self.__model.resume()
         self.__view.suspendResumeButton.set_label("Suspend")
-        #self.__view.suspendResumeButton.disconnect(self.__suspendResumeHandler)
-        #self.__suspendResumeHandler = self.__view.suspendResumeButton.connect('clicked', self.__onSuspendButtonClicked)
-        
-    #def __onResumeButtonClicked(self, widget):
-        #""" Resume button has been clicked.
-        #"""
-        #Logger().trace("ShootController.__resumeButtonClicked()")
-        #self.__resumeShooting()
-    
-    def __onSuspendResumeButtonClicked(self, widget):
-        """ SuspendResume button has been clicked.
-        """
-        Logger().trace("ShootController.__suspendResumeButtonClicked()")
-        if self.__model.isShooting(): # Should always be true here, but...
-            if not self.__model.isSuspended():
-                self.__suspendShooting()
-            else:
-                self.__resumeShooting()
     
     def __stopShooting(self):
         self.__model.stop()
@@ -330,18 +322,6 @@ class ShootController(AbstractController):
         # todo: use condition
         while self.__model.isShooting():
             time.sleep(0.1)
-        
-    def __onStopButtonClicked(self, widget):
-        """ Stop button has been clicked.
-        """
-        Logger().trace("ShootController.__stopButtonClicked()")
-        self.__stopShooting()
-        
-    def __onDoneButtonClicked(self, widget):
-        """ Done button has been clicked.
-        """
-        Logger().trace("ShootController.__onDoneButtonClicked()")
-        #self.__view.shootDialog.response(0)
 
     def __refreshPos(self, yaw, pitch):
         """ Refresh position according to new pos.
