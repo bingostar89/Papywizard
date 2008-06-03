2dcb00a0ec38e0cc1a0abf7a1bc383f806e956ee
fma <fma@1bb8f0fc-47fd-4586-b4e1-c5ba9e517023>
2008-06-03 13:22:56 +0000

    Added meta-data

--- setup.py
+++ setup.py
@@ -1,20 +1,27 @@
 #import bdist_debian
 from distutils.core import setup
 
-setup(name='papywizard',
-      scripts=['papywizard'],
-      version='0.9 beta 2',
-      #section='user/backup',
-      maintainer='Frederic Mantegazza',
-      maintainer_email='Frederic Mantegazza <frederic.mantegazza@gbiloba.org>',
-      #depends='python2.5, python2.5-hildon, python2.5-gtk2',
+setup(name="papywizard",
+      version="0.9 beta 2",
+      author="Frederic Mantegazza",
+      author_email="Frederic Mantegazza <frederic.mantegazza@gbiloba.org>",
+      maintainer="Frederic Mantegazza",
+      maintainer_email="Frederic Mantegazza <frederic.mantegazza@gbiloba.org>",
+      url="http://trac.gbiloba.org/papywizard",
       description="Merlin/Orion panohead control software",
-      package_dir={'papywizard': '.'},
-      packages=['papywizard', 'papywizard.common', 'papywizard.model',
-                'papywizard.controller', 'papywizard.hardware',
-                'papywizard.view', 'papywizard.view3D'],
-      package_data={'papywizard': ['view/*.glade']}
-      #icon='papywizard.png',
-      #cmdclass={'bdist_debian': bdist_debian.bdist_debian}
+      #long_description="",
+      download_url="http://trac.gbiloba.org/papywizard/wiki/WikiStart#Download",
+      scripts=["papywizard"],
+      package_dir={'papywizard': "."},
+      packages=["papywizard", "papywizard.common", "papywizard.model",
+                "papywizard.controller", "papywizard.hardware",
+                "papywizard.view", "papywizard.view3D"],
+      package_data={'papywizard': ["view/*.glade"]},
+
+      # pymaemo stuff
+      #section="user/backup",
+      #depends="python2.5, python2.5-hildon, python2.5-gtk2",
+      #icon="papywizard.png",
+      #cmdclass={'bdist_debian': bdist_debian.bdist_debian},
   )
 
