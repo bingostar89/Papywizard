420f780a3f195d59d9a87857941b8123f0026502
fma <fma@1bb8f0fc-47fd-4586-b4e1-c5ba9e517023>
2008-06-05 07:21:09 +0000

    Added get/setInt/Float/Boolean() functions

--- common/configManager.py
+++ common/configManager.py
@@ -17,11 +17,13 @@ Implements class:
 __revision__ = "$Id$"
 
 import os.path
+import sets
 import ConfigParser
 
 from papywizard.common import config
-from papywizard.common.loggingServices import Logger
+#from papywizard.common.loggingServices import Logger
 
+CONFIG_VERSION = 1
 
 class ConfigManager(object):
     """ ConfigManager.
@@ -42,21 +44,26 @@ class ConfigManager(object):
         @todo: update user config. from global config. instead of overwriting
         """
         if ConfigManager.__init:
+            #Logger().debug("ConfigManager.__init__()")
+            print "ConfigManager.__init__()"
             globalConfig = ConfigParser.SafeConfigParser()
-            globalConfig.read(config.GLOBAL_CONFIG_FILE)
-            globalConfigVersion = globalConfig.get('General', 'VERSION')
+            if globalConfig.read(config.GLOBAL_CONFIG_FILE) == []:
+                if globalConfig.read(config.CONFIG_FILE) == []:
+                    raise IOError("Can't read configuration file")
             userConfig = ConfigParser.SafeConfigParser()
             
             # Check if user config. exists
             if userConfig.read(config.USER_CONFIG_FILE) == []:
-                Logger().warning("User config. does not exist; copying from global config.")
+                #Logger().warning("User config. does not exist; copying from global config.")
+                print "User config. does not exist; copying from global config."
                 globalConfig.write(file(config.USER_CONFIG_FILE, 'w'))
                 userConfig.read(config.USER_CONFIG_FILE)
 
             # Check if user config. needs to be updated
-            userConfigVersion = userConfig.get('General', 'VERSION')
-            if userConfigVersion != globalConfigVersion:
-                Logger().warning("User config. has wrong version.; updating from global config.")
+            userConfigVersion = userConfig.getint('General', 'CONFIG_VERSION')
+            if userConfigVersion != CONFIG_VERSION:
+                #Logger().warning("User config. has wrong version.; updating from global config.")
+                print "User config. has wrong version.; updating from global config."
                 
                 # Remove obsolete sections
                 globalSections = globalConfig.sections()
@@ -87,19 +94,10 @@ class ConfigManager(object):
     def save(self):
         """ Save config.
         
-        Config is saved in user config. directory
+        Config is saved in user directory. Preferences are first
+        set back to config.
         """
         self.__config.write(file(config.USER_CONFIG_FILE, 'w'))
-
-    def getPreferences(self):
-        """ Get the current preferences.
-        
-        Preferences are configs related to the model.
-        
-        @return: current preferences
-        @rtype: dict
-        """
-        return dict(self.__config.items('Preferences'))
 
     def getDefaultPreferences(self):
         """ Get the default preferences.
@@ -121,6 +119,39 @@ class ConfigManager(object):
         @type option: str
         """
         return self.__config.get(section, option)
+
+    def getInt(self, section, option):
+        """ Get a value.
+        
+        @param section: section name to use
+        @type section: str
+        
+        @param option: option to get value from
+        @type option: int
+        """
+        return self.__config.getint(section, option)
+
+    def getFloat(self, section, option):
+        """ Get a value.
+        
+        @param section: section name to use
+        @type section: str
+        
+        @param option: option to get value from
+        @type option: float
+        """
+        return self.__config.getfloat(section, option)
+
+    def getBoolean(self, section, option):
+        """ Get a value.
+        
+        @param section: section name to use
+        @type section: str
+        
+        @param option: option to get value from
+        @type option: bool
+        """
+        return self.__config.getboolean(section, option)
         
     def set(self, section, option, value):
         """ Set a value.
@@ -135,4 +166,49 @@ class ConfigManager(object):
         @type value: str
         """
         self.__config.set(section, option, value)
+        
+    def setInt(self, section, option, value):
+        """ Set a value as int.
+        
+        @param section: section name to use
+        @type section: str
+        
+        @param option: option to set value to
+        @type option: str
+        
+        @param value: value to set
+        @type value: int
+        """
+        self.__config.set(section, option, "%d" % value)
+        
+    def setFloat(self, section, option, value, prec):
+        """ Set a value as float.
+        
+        @param section: section name to use
+        @type section: str
+        
+        @param option: option to set value to
+        @type option: str
+        
+        @param value: value to set
+        @type value: float
+        
+        @param prec: precision
+        @type prec: int
+        """
+        self.__config.set(section, option, ("%(format)s" % {'format': "%%.%df" % prec}) % value)
+        
+    def setBoolean(self, section, option, value):
+        """ Set a value.
+        
+        @param section: section name to use
+        @type section: str
+        
+        @param option: option to set value to
+        @type option: str
+        
+        @param value: value to set
+        @type value: str
+        """
+        self.__config.set(section, option, str(value))
         
