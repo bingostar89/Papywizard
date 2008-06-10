dfa83c4fe95504f78b9923d795a536fb51439be1
fma <fma@1bb8f0fc-47fd-4586-b4e1-c5ba9e517023>
2008-06-10 06:41:06 +0000

    Load correct glade file

--- view/configDialog.py
+++ view/configDialog.py
@@ -35,7 +35,7 @@ class ConfigDialog(object):
         """ Init the object.
         """
         # Set the Glade file
-        gladeFile = os.path.join(path, "configDialog-new.glade")
+        gladeFile = os.path.join(path, "configDialog.glade")
         self.wTree = gtk.glade.XML(gladeFile)
 
         # Retreive usefull widgets
