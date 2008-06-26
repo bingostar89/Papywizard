6e30344d5295b4d1c235d7e909b09f7fe537f8df
fma <fma@1bb8f0fc-47fd-4586-b4e1-c5ba9e517023>
2008-06-26 06:04:53 +0000

    First commit

---
+++ view/connectDialog.py
@@ -0,0 +1,86 @@
+# -*- coding: iso-8859-1 -*-
+
+""" Panohead remote control.
+
+License
+=======
+
+ - B{papywizard} (U{http://trac.gbiloba.org/papywizard}) is Copyright:
+  - (C) 2007-2008 Frédéric Mantegazza
+
+This software is governed by the B{CeCILL} license under French law and
+abiding by the rules of distribution of free software.  You can  use, 
+modify and/or redistribute the software under the terms of the CeCILL
+license as circulated by CEA, CNRS and INRIA at the following URL
+U{http://www.cecill.info}.
+
+As a counterpart to the access to the source code and  rights to copy,
+modify and redistribute granted by the license, users are provided only
+with a limited warranty  and the software's author,  the holder of the
+economic rights,  and the successive licensors  have only  limited
+liability. 
+
+In this respect, the user's attention is drawn to the risks associated
+with loading,  using,  modifying and/or developing or reproducing the
+software by the user in light of its specific status of free software,
+that may mean  that it is complicated to manipulate,  and  that  also
+therefore means  that it is reserved for developers  and  experienced
+professionals having in-depth computer knowledge. Users are therefore
+encouraged to load and test the software's suitability as regards their
+requirements in conditions enabling the security of their systems and/or 
+data to be ensured and,  more generally, to use and operate it in the 
+same conditions as regards security. 
+
+The fact that you are presently reading this means that you have had
+knowledge of the CeCILL license and that you accept its terms.
+
+Module purpose
+==============
+
+View
+
+Implements
+==========
+
+- ConnectDialog
+
+@author: Frédéric Mantegazza
+@copyright: (C) 2007-2008 Frédéric Mantegazza
+@license: CeCILL
+"""
+
+__revision__ = "$Id: configDialog.py 291 2008-06-23 06:29:48Z fma $"
+
+import os.path
+
+#import pygtk
+#pygtk.require("2.0")
+import gtk.glade
+
+path = os.path.dirname(__file__)
+
+from papywizard.common.loggingServices import Logger
+
+
+class ConnectDialog(object):
+    """ Connection dialog.
+    """
+    def __init__(self):
+        """ Init the object.
+        """
+        # Set the Glade file
+        gladeFile = os.path.join(path, "connectDialog.glade")
+        self.wTree = gtk.glade.XML(gladeFile)
+
+        # Retreive usefull widgets
+        self._retreiveWidgets()
+
+    def _retreiveWidgets(self):
+        """ Get widgets from widget tree.
+        """
+        self.connectDialog = self.wTree.get_widget("connectDialog")
+        self.progressbar = self.wTree.get_widget("progressbar")
+
+    def fillWidgets(self, values):
+        """ Fill widgets with values.
+        """
