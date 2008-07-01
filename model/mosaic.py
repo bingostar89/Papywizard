15d4465d4d2c1fab942e30d84c88d28a55a6f282
fma <fma@1bb8f0fc-47fd-4586-b4e1-c5ba9e517023>
2008-07-01 14:14:21 +0000

    Moved mosaic params to Mosaic object First step to path custom

--- model/mosaic.py
+++ model/mosaic.py
@@ -53,111 +53,305 @@ __revision__ = "$Id$"
 
 from papywizard.common.loggingServices import Logger
 from papywizard.common.configManager import ConfigManager
+from papywizard.model.scan import Scan
 
 
-class Mosaic(object):
+class Mosaic(Scan):
     """ Mosaic model.
-
-    This is an iterator which iters of positions to shoot. A
-    template defines the way positions are generated.
-    Use it just as any iterator:
-
-    >>> mosaic = Mosaic()
-    >>> mosaic.template = "Y+P+Y-"
-    >>> mosaic.setMatrix(yaw=3, pitch=3)
-    >>> for yaw, pitch in mosaic:
-    ...     print yaw, pitch
-    0 0
-    1 0
-    2 0
-    2 1
-    1 1
-    0 1
-    0 2
-    1 2
-    2 2
     """
-    def __init__(self, yawNbPicts, pitchNbPicts):
+    def __init__(self, camera):
         """ Init the Mosaic object.
-
-        @param yawNbPicts: number of pictures for yaw direction
-        @type yawNbPicts: int
-
-        @param pitchNbPicts: number of pictures for pitch direction
-        @type pitchNbPicts: int
+        
+        @param camera: camera object
+        @type camera: {Camera}
         """
-        self.__yawNbPicts = yawNbPicts
-        self.__pitchNbPicts = pitchNbPicts
+        self.__camera = camera
+        
+        self.yawStart = 0.
+        self.pitchStart = 0.
+        self.yawEnd = 0.
+        self.pitchEnd = 0.
+        self.__yawIndex = None
+        self.__pitchIndex = None
+        self.__yawInc = None
+        self.__pitchInc = None
+        self.__yawIndex = None
+        self.__pitchIndex = None
+
+        #self.__yawSens = None
+        #self.__pitchSens = None
+        
 
     def __iter__(self):
         """ Define Mosaic as an iterator.
         """
+        yawCameraFov = self.__camera.getYawFov(self.cameraOrientation)
+        pitchCameraFov = self.__camera.getPitchFov(self.cameraOrientation)
+        try:
+            self.__yawInc = (self.yawFov - yawCameraFov) / (self.yawNbPicts - 1)
+        except ZeroDivisionError:
+            self.__yawInc = self.yawFov - yawCameraFov
+        try:
+            self.__pitchInc = (self.pitchFov - pitchCameraFov) / (self.pitchNbPicts - 1)
+        except ZeroDivisionError:
+            self.__pitchInc = self.pitchFov - pitchCameraFov
+        self.__yawInc *= cmp(self.yawEnd, self.yawStart)
+        self.__pitchInc *= cmp(self.pitchEnd, self.pitchStart)
 
-        # Init self.__yaw and self.__pitch according of the template (todo)
-        self.__inc = [{'yaw': 1, 'pitch': 0},
-                      {'yaw': 0, 'pitch': 1},
-                      {'yaw': -1, 'pitch': 0},
-                      {'yaw': 0, 'pitch': 1}
-                      ]
-        self.__start = {'yaw': 0, 'pitch': 0}
-        self.__yaw = -1
-        self.__yawInc = 1
-        self.__pitch = 0
-        self.__pitchInc = 1
+        # Init mosaic shooting params (should be in preferences)
+        self.__firstMove = "yaw"
+        self.__yawCR = False
+        self.__pitchCR = False
+        
+        #if self.__yawInc > 0:
+            #self.__yawIndex = 0
+            #self.__yawSens = 1
+        #else:
+            #self.__yawIndex = self.yawNbPicts - 1
+            #self.__yawSens = -1
+        #if self.__pitchInc > 0:
+            #self.__pitchIndex = 0
+            #self.__pitchSens = 1
+        #else:
+            #self.__pitchIndex = self.pitchNbPicts - 1
+            #self.__pitchSens = -1
+            
+        self.__yawIndex = 0
+        self.__yawSens = 1
+        self.__pitchIndex = 0
+        self.__pitchSens = 1
+            
+        return self.next2()
 
-        return self
-
-    #def generate(self, yawNbPicts, pitchNbPicts):
-        #if not self.__init:
-            #inc = [{'yaw': 1, 'pitch': 0},
-                   #{'yaw': 0, 'pitch': 1},
-                   #{'yaw': -1, 'pitch': 0},
-                   #{'yaw': 0, 'pitch': 1}
-                  #]
-            #yaw = 0
-            #pitch = 0
-            #idx = 0
-            #self.__init = True
-
-        #yield yaw, pitch
-        #yaw += inc[idx]['yaw']
-        #pitch += inc[idx]['pitch']
-        #if yaw >= yawNbPicts:
-
-    def next(self):
+    def next2(self):
         """ Return next (yaw, pitch) index position.
         """
-        self.__yaw += self.__yawInc
-        if self.__yaw >= self.__yawNbPicts:
-            self.__yaw = self.__yawNbPicts - 1
-            self.__yawInc = -1
-            self.__pitch += self.__pitchInc
-            if self.__pitch >= self.__pitchNbPicts:
-                self.__yawNbPicts = self.__pitchNbPicts = None
-                raise StopIteration
-            elif self.__pitch < 0:
-                self.__yawNbPicts = self.__pitchNbPicts = None
-                raise StopIteration
-        elif self.__yaw < 0:
-            self.__yaw = 0
-            self.__yawInc = +1
-            self.__pitch += self.__pitchInc
-            if self.__pitch >= self.__pitchNbPicts:
-                self.__yawNbPicts = self.__pitchNbPicts = None
-                raise StopIteration
-            elif self.__pitch < 0:
-                self.__yawNbPicts = self.__pitchNbPicts = None
-                raise StopIteration
-        Logger().debug("Mosaic.next(): __yaw=%d, __pitch=%d" % (self.__yaw, self.__pitch))
-        return self.__yaw, self.__pitch
+        while True:
+            yaw = self.yawStart + self.__yawIndex * self.__yawInc
+            pitch = self.pitchStart + self.__pitchIndex * self.__pitchInc
+            Logger().debug("Mosaic.next(): __yawIndex=%d, __pitchIndex=%d" % (self.__yawIndex, self.__pitchIndex))
+            Logger().debug("Mosaic.next(): yaw=%.1f, pitch=%.1f" % (yaw, pitch))
+            yield yaw, pitch
+            
+            # Compute next position
+            if self.__firstMove == "yaw":
+                self.__yawIndex += self.__yawSens
+            elif self.__firstMove == "pitch":
+                self.__pitchIndex += self.__pitchSens
 
+            for i in xrange(2):
+                if self.__yawIndex == self.yawNbPicts: # __yawSens was 1
+                    if self.__firstMove == "pitch":
+                        raise StopIteration
+                    if self.__yawCR:
+                        self.__yawIndex = 0
+                        self.__yawSens = 1
+                    else:
+                        self.__yawIndex = self.yawNbPicts - 1
+                        self.__yawSens = -1
+                    self.__pitchIndex += self.__pitchSens
+                    continue
+                elif self.__yawIndex == -1:            # __yawSens was -1
+                    if self.__firstMove == "pitch":
+                        raise StopIteration
+                    if self.__yawCR:
+                        self.__yawIndex = self.yawNbPicts - 1
+                        self.__yawSens = -1
+                    else:
+                        self.__yawIndex = 0
+                        self.__yawSens = 1
+                    self.__pitchIndex += self.__pitchSens
+                    continue
+                
+                if self.__pitchIndex == self.pitchNbPicts: # __pitchSens was 1
+                    if self.__firstMove == "yaw":
+                        raise StopIteration
+                    if self.__pitchCR:
+                        self.__pitchIndex = 0
+                        self.__pitchSens = 1
+                    else:
+                        self.__pitchIndex = self.pitchNbPicts - 1
+                        self.__pitchSens = -1
+                    self.__yawIndex += self.__yawSens
+                    continue
+                elif self.__pitchIndex == -1:              # __pitchSens was -1
+                    if self.__firstMove == "yaw":
+                        raise StopIteration
+                    if self.__pitchCR:
+                        self.__pitchIndex = self.pitchNbPicts - 1
+                        self.__pitchSens = -1
+                    else:
+                        self.__pitchIndex = 0
+                        self.__pitchSens = 1
+                    self.__yawIndex += self.__yawSens
+                    continue
+                
+                break
 
-def main():
-    mosaic = Mosaic(3, 3)
-    for i, (yaw, pitch) in enumerate(mosaic):
-        print "index=%d, yawCoef=%d, pitchCoef=%d" % (i, yaw, pitch)
+    #def next(self):
+        #""" Return next (yaw, pitch) index position.
+        #"""
+        #self.__yawIndex += self.__yawSens
+        #if self.__yawIndex >= self.yawNbPicts:
+            #self.__yawIndex = self.yawNbPicts - 1
+            #self.__yawSens = -1
+            #self.__pitchIndex += self.__pitchSens
+            #if self.__pitchIndex >= self.pitchNbPicts:
+                #raise StopIteration
+            #elif self.__pitchIndex < 0:
+                #raise StopIteration
+        #elif self.__yawIndex < 0:
+            #self.__yawIndex = 0
+            #self.__yawSens = +1
+            #self.__pitchIndex += self.__pitchSens
+            #if self.__pitchIndex >= self.pitchNbPicts:
+                #raise StopIteration
+            #elif self.__pitchIndex < 0:
+                #raise StopIteration
+            
+        #yaw = self.yawStart + self.__yawIndex * self.__yawInc
+        #pitch = self.pitchStart + self.__pitchIndex * self.__pitchInc
+        #Logger().debug("Mosaic.next(): __yawIndex=%d, __pitchIndex=%d" % (self.__yawIndex, self.__pitchIndex))
+        #Logger().debug("Mosaic.next(): yaw=%.1f, pitch=%.1f" % (yaw, pitch))
         
+        #return yaw, pitch
 
-if __name__ == "__main__":
-    main()
+    # Properties
+    def __getTemplate(self):
+        """
+        """
+        return ConfigManager().get('Preferences', 'SHOOTING_MOSAIC_TEMPLATE')
     
+    def __setTemplate(self, template):
+        """
+        """
+        ConfigManager().set('Preferences', 'SHOOTING_MOSAIC_TEMPLATE', template)
+
+    template = property(__getTemplate, __setTemplate)
+    
+    def __getCameraOrientation(self):
+        """
+        """
+        return ConfigManager().get('Preferences', 'SHOOTING_MOSAIC_CAMERA_ORIENTATION')
+    
+    def __setCameraOrientation(self, cameraOrientation):
+        """
+        """
+        ConfigManager().set('Preferences', 'SHOOTING_MOSAIC_CAMERA_ORIENTATION', cameraOrientation)
+
+    cameraOrientation = property(__getCameraOrientation, __setCameraOrientation)
+    
+    def __getOverlap(self):
+        """
+        """
+        return ConfigManager().getFloat('Preferences', 'SHOOTING_MOSAIC_OVERLAP')
+    
+    def __setOverlap(self, overlap):
+        """
+        """
+        ConfigManager().setFloat('Preferences', 'SHOOTING_MOSAIC_OVERLAP', overlap, 2)
+        
+    overlap = property(__getOverlap, __setOverlap)
+
+    def __getYawFov(self):
+        """
+        """
+        yawCameraFov = self.__camera.getYawFov(self.cameraOrientation)
+        return abs(self.yawEnd - self.yawStart) + yawCameraFov
+
+    yawFov = property(__getYawFov, "Total yaw FoV")
+
+    def __getPitchFov(self):
+        """
+        """
+        pitchCameraFov = self.__camera.getPitchFov(self.cameraOrientation)
+        return abs(self.pitchEnd - self.pitchStart) + pitchCameraFov
+
+    pitchFov = property(__getPitchFov, "Total pitch FoV")
+
+    def __getYawNbPicts(self):
+        """
+        """
+        yawCameraFov = self.__camera.getYawFov(self.cameraOrientation)
+        if round(self.yawFov - yawCameraFov, 1) >= 0.1:
+            nbPicts = int(((self.yawFov - self.overlap * yawCameraFov) / (yawCameraFov * (1 - self.overlap))) + 1)
+        else:
+            nbPicts = 1
+        return nbPicts
+
+    yawNbPicts = property(__getYawNbPicts, "Yaw nb picts")
+
+    def __getPitchNbPicts(self):
+        """
+        """
+        pitchCameraFov = self.__camera.getPitchFov(self.cameraOrientation)
+        if round(self.pitchFov - pitchCameraFov, 1) >= 0.1:
+           nbPicts = int(((self.pitchFov - self.overlap * pitchCameraFov) / (pitchCameraFov * (1 - self.overlap))) + 1)
+        else:
+            nbPicts = 1
+        return nbPicts
+
+    pitchNbPicts = property(__getPitchNbPicts, "Pitch nb picts")
+
+    def __getTotalNbPicts(self):
+        """
+        """
+        return self.yawNbPicts * self.pitchNbPicts
+    
+    totalNbPicts = property(__getTotalNbPicts)
+
+    def __getYawRealOverlap(self):
+        """ Recompute real yaw overlap.
+        """
+        yawCameraFov = self.__camera.getYawFov(self.cameraOrientation)
+        if self.yawNbPicts > 1:
+            overlap = (self.yawNbPicts * yawCameraFov - self.yawFov) / (yawCameraFov * (self.yawNbPicts - 1))
+        else:
+            overlap = 1.
+        return overlap
+
+    yawRealOverlap = property(__getYawRealOverlap, "Yaw real overlap")
+
+    def __getPitchRealOverlap(self):
+        """ Recompute real pitch overlap.
+        """
+        pitchCameraFov = self.__camera.getPitchFov(self.cameraOrientation)
+        if self.pitchNbPicts > 1:
+            overlap = (self.pitchNbPicts * pitchCameraFov - self.pitchFov) / (pitchCameraFov * (self.pitchNbPicts - 1))
+        else:
+            overlap = 1.
+        return overlap
+
+    pitchRealOverlap = property(__getPitchRealOverlap, "Pitch real overlap")
+
+    def __totalNbPicts(self):
+        return self.__yawNbPicts * self.__pitchNbPicts
+
+    # Public methods
+    def storeStartPosition(self, yaw, pitch):
+        """ Store current position as start position.
+        """
+        self.yawStart, self.pitchStart = yaw, pitch
+        Logger().debug("Shooting.storeStartPosition(): yaw=%.1f, pitch=%.1f" % (self.yawStart, self.pitchStart))
+
+    def storeEndPosition(self, yaw, pitch):
+        """ Store current position as end position.
+        """
+        self.yawEnd, self.pitchEnd = yaw, pitch
+        Logger().debug("Shooting.storeEndPosition(): yaw=%.1f, pitch=%.1f" % (self.yawEnd, self.pitchEnd))
+        
+    def setYaw360(self, yaw):
+        """ Compute start/end yaw position for 360°.
+        """
+        yawCameraFov = self.__camera.getPitchFov(self.cameraOrientation)
+        self.yawStart = yaw - 180. + yawCameraFov * (1 - self.overlap) / 2.
+        self.yawEnd = yaw + 180. - yawCameraFov * (1 - self.overlap) / 2.
+        Logger().debug("Mosaic.setYaw360(): startYaw=%.1f, endYaw=%.1f" % (self.yawStart, self.yawEnd))
+
+    def setPitch180(self, pitch):
+        """ Compute start/end pitch position for 180°.
+        """
+        pitchCameraFov = self.__camera.getPitchFov(self.cameraOrientation)
+        self.pitchStart = pitch - 90. + pitchCameraFov * (1 - self.overlap) / 2.
+        self.pitchEnd = pitch + 90. - pitchCameraFov * (1 - self.overlap) / 2.
+        Logger().debug("Mosaic.setPitch180(): startPitch=%.1f, endPitch=%.1f" % (self.pitchStart, self.pitchEnd))
