2f69bc0c78bf40f094bd2061dd5cf7afb63a0361
fma <fma@1bb8f0fc-47fd-4586-b4e1-c5ba9e517023>
2008-09-16 14:04:22 +0000

    Bug correction in data file format config

--- papywizard/common/data.py
+++ papywizard/common/data.py
@@ -9,7 +9,7 @@ License
   - (C) 2007-2008 Frédéric Mantegazza
 
 This software is governed by the B{CeCILL} license under French law and
-abiding by the rules of distribution of free software.  You can  use, 
+abiding by the rules of distribution of free software.  You can  use,
 modify and/or redistribute the software under the terms of the CeCILL
 license as circulated by CEA, CNRS and INRIA at the following URL
 U{http://www.cecill.info}.
@@ -18,7 +18,7 @@ As a counterpart to the access to the source code and  rights to copy,
 modify and redistribute granted by the license, users are provided only
 with a limited warranty  and the software's author,  the holder of the
 economic rights,  and the successive licensors  have only  limited
-liability. 
+liability.
 
 In this respect, the user's attention is drawn to the risks associated
 with loading,  using,  modifying and/or developing or reproducing the
@@ -27,9 +27,9 @@ that may mean  that it is complicated to manipulate,  and  that  also
 therefore means  that it is reserved for developers  and  experienced
 professionals having in-depth computer knowledge. Users are therefore
 encouraged to load and test the software's suitability as regards their
-requirements in conditions enabling the security of their systems and/or 
-data to be ensured and,  more generally, to use and operate it in the 
-same conditions as regards security. 
+requirements in conditions enabling the security of their systems and/or
+data to be ensured and,  more generally, to use and operate it in the
+same conditions as regards security.
 
 The fact that you are presently reading this means that you have had
 knowledge of the CeCILL license and that you accept its terms.
@@ -154,19 +154,21 @@ class Data(object):
         """
         if ConfigManager().getBoolean('Data', 'DATA_FILE_ENABLE'):
             Logger().trace("Data.serialize()")
-            filename = os.path.join(config.HOME_DIR, config.DATA_FILE)
-            xmlFile = file(filename % self._date, 'w')
+            fileFormat = os.path.join(config.HOME_DIR, ConfigManager().get('Data', 'DATA_FILE_FORMAT'))
+            print fileFormat
+            dataFileFormatDict = {'date': self._date}
+            xmlFile = file(fileFormat % dataFileFormatDict, 'w')
             self._doc.writexml(xmlFile, addindent="    ", newl='\n', encoding="utf-8")
             xmlFile.close()
 
     def createHeader(self, values):
         """ Create the header.
-        
+
         @param values: values to put in the header
         @type values: dict
         """
         Logger().debug("Data.createHeader(): values=%s" % values)
-        
+
         # Shooting
         node = self._addNode(self._headerNode, 'shooting')
         self._addNode(node, 'stabilizationDelay', values['stabilizationDelay'])
@@ -174,18 +176,18 @@ class Data(object):
                                        yaw=values['yawRealOverlap'],
                                        pitch=values['pitchRealOverlap'])
         self._addNode(node, 'cameraOrientation', values['cameraOrientation'])
-        
+
         # Camera
         node = self._addNode(self._headerNode, 'camera')
         self._addNode(node, 'timeValue', values['timeValue'])
         self._addNode(node, 'nbPicts', values['nbPicts'])
         self._addNode(node, 'sensor', coef=values['sensorCoef'],
                                       ratio=values['sensorRatio'])
-        
+
         # Lens
         node = self._addNode(self._headerNode, 'lens')
         self._addNode(node, 'focal', values['focal'])
-        
+
         # Mode
         node = self._addNode(self._headerNode, 'mode', name=values['mode'])
         self._addNode(node, 'nbPicts', yaw=values['yawNbPicts'],
