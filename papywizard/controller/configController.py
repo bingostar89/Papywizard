367bcf9391bf1e708b937402e6d3829f96bf2d63
fma <fma@1bb8f0fc-47fd-4586-b4e1-c5ba9e517023>
2008-09-18 12:12:10 +0000

    Added log view button callback

--- papywizard/controller/configController.py
+++ papywizard/controller/configController.py
@@ -56,17 +56,31 @@ from papywizard.common.configManager import ConfigManager
 from papywizard.common.loggingServices import Logger
 from papywizard.controller.abstractController import AbstractController
 from papywizard.controller.bluetoothChooserController import BluetoothChooserController
+from papywizard.controller.loggerController import LoggerController
+from papywizard.view.logBuffer import LogBuffer
 
 
 class ConfigController(AbstractController):
     """ Configuration controller object.
     """
+    def __init__(self, parent, model):
+        """ Init the controller.
+
+        @param serializer: object used to serialize toolkit events
+        @type serializer: {Serializer}
+        """
+        super(ConfigController, self).__init__(parent, model)
+
+        self.__gtkLogStream = LogBuffer()
+        Logger().addStreamHandler(self.__gtkLogStream)
+
     def _init(self):
         self._gladeFile = "configDialog.glade"
         self._signalDict = {"on_okButton_clicked": self.__onOkButtonClicked,
                             "on_cancelButton_clicked": self.__onCancelButtonClicked,
                             "on_driverCombobox_changed": self.__onDriverComboboxChanged,
-                            "on_chooseBluetoothButton_clicked": self.__onChooseBluetoothButtonClicked
+                            "on_chooseBluetoothButton_clicked": self.__onChooseBluetoothButtonClicked,
+                            "on_viewLogButton_clicked": self.__onViewLogButtonClicked,
                         }
 
     def _retreiveWidgets(self):
@@ -168,6 +182,16 @@ class ConfigController(AbstractController):
                             (address, name))
             self.bluetoothDeviceAddressEntry.set_text(address)
 
+    def __onViewLogButtonClicked(self, widget):
+        """ View log button clicked.
+        """
+        Logger().trace("ConfigController.__onViewLogButtonClicked()")
+        controller = LoggerController(self, self._model)
+        controller.loggerTextview.set_buffer(self.__gtkLogStream)
+        controller.run()
+        controller.destroyView()
+
+    # Real work
     def refreshView(self):
         self.stabilizationDelaySpinbutton.set_value(self._model.stabilizationDelay)
         self.cameraOrientationCombobox.set_active(config.CAMERA_ORIENTATION_INDEX[self._model.mosaic.cameraOrientation])
