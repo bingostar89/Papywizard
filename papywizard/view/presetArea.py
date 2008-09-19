6aebeb59242830099b602da1964d7fd9d3f14e89
fma <fma@1bb8f0fc-47fd-4586-b4e1-c5ba9e517023>
2008-09-19 11:17:40 +0000

    Design review Better display

--- papywizard/view/presetArea.py
+++ papywizard/view/presetArea.py
@@ -62,50 +62,59 @@ class PresetArea(ShootingArea):
     """ GTK PresetArea widget
     """
     def __init__(self):
-        """ Init PresetArea widget.
-        """
         ShootingArea.__init__(self)
 
+        self.__yawMargin = None
+        self.__pitchMargin = None
+
     # Callbacks
+    def _configure_cb(self, widget, event):
+        ShootingArea._configure_cb(self, widget, event)
+
+        self.__yawMargin = int(round((self._width - 360. * self._scale) / 2.))
+        self.__pitchMargin = int(round((self._height - 180. * self._scale) / 2.))
+        #print "yawMargin=%d, pitchMargin=%d" % (self.__yawMargin, self.__pitchMargin)
+
+        return True
+
     def _expose_cb(self, widget, event):
 
         # Draw background
-        xBack = int(round(self._yawOffset))
-        yBack = int(round(self._pitchOffset))
-        wBack = self._width - int(round(2 * self._yawOffset))
-        hBack = self._height - int(round(2 * self._pitchOffset))
-        self.window.draw_rectangle(self._back, True, xBack, yBack, wBack, hBack)
-        #print "xBack=%d, yBack=%d, wBack=%d, hBack=%d" % (xBack, yBack, wBack, hBack)
-                                                
-        # Draw picts
-        for i, (yaw, pitch) in enumerate(self._picts):
-            #if cmp(self._yawEnd, self._yawStart) > 0:
-                #yaw -= self._yawStart
-            #else:
-                #yaw -= self._yawEnd
-            #if cmp(self._pitchEnd, self._pitchStart) > 0:
-                #pitch -= self._pitchStart
-            #else:
-                #pitch -= self._pitchEnd
-            x = int(round(yaw * self._scale)) + int(round(self._yawOffset)) + int(round(self._yawCameraFov * self._scale / 2.))
-            y = int(round(pitch * self._scale)) + int(round(self._pitchOffset))
-            w = int(round(self._yawCameraFov * self._scale))
-            h = int(round(self._pitchCameraFov * self._scale))
-            y = self._height - y - h
-            #print "pict=%d, yaw=%.1f, pitch=%.1f, x=%.1f, y=%.1f, w=%.1f, h=%.1f" % (i + 1, yaw, pitch, x, y, w, h)
-            self.window.draw_rectangle(self._fg1, True, x, y, w, h)
-            x += 1
-            y += 1
-            w -= 2
-            h -= 2
-            self.window.draw_rectangle(self._fg2, True, x, y, w, h)
-            
-        # Draw 360°x180° area
-        xFull = int(round(self._width / 2.)) - int(round(180 * self._scale))
-        yFull = int(round(self._height / 2.)) - int(round(90 * self._scale))
+        self.window.draw_rectangle(self._back, True, 0, 0, self._width, self._height)
+
+        # Draw 360°x180° area and axis
+        xFull = int(round(self._width / 2. - 180 * self._scale))
+        yFull = int(round(self._height / 2. - 90 * self._scale))
         wFull = int(round(360 * self._scale)) - 1
         hFull = int(round(180 * self._scale)) - 1
         #print "xFull=%.1f, yFull=%.1f, wFull=%.1f, hFull=%.1f" % (xFull, yFull, wFull, hFull)
         self.window.draw_rectangle(self._fg3, False, xFull, yFull, wFull, hFull)
-        
+        x1 = 0
+        y1 = int(round(self._height / 2.)) + 1
+        x2 = self._width
+        y2 = y1
+        self.window.draw_line(self._fg3, x1, y1, x2, y2)
+        x1 = self.__yawMargin
+        y1 = 0
+        x2 = x1
+        y2 = self._height
+        self.window.draw_line(self._fg3, x1, y1, x2, y2)
+
+        # Draw picts
+        for i, (yaw, pitch) in enumerate(self._picts):
+            pitch = 180 / 2. - pitch
+            x = int(round(yaw * self._scale - self._yawCameraFov * self._scale / 2.)) + self.__yawMargin
+            y = int(round(pitch * self._scale - self._pitchCameraFov * self._scale / 2.)) + self.__pitchMargin
+            w = int(round(self._yawCameraFov * self._scale))
+            h = int(round(self._pitchCameraFov * self._scale))
+            #print "pict=%d, yaw=%.1f, pitch=%.1f, x=%.1f, y=%.1f, w=%.1f, h=%.1f" % (i + 1, yaw, pitch, x, y, w, h)
+            #self.window.draw_rectangle(self._fg1, True, x, y, w, h)
+            self.window.draw_arc(self._fg1, True, x, y, w, h, 0, 360 * 64)
+            x += 1
+            y += 1
+            w -= 2
+            h -= 2
+            #self.window.draw_rectangle(self._fg2, True, x, y, w, h)
+            self.window.draw_arc(self._fg2, True, x, y, w, h, 0, 360 * 64)
+
         return False
