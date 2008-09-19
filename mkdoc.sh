ebb188d0805c093c631c98480c671c62761be97f
fma <fma@1bb8f0fc-47fd-4586-b4e1-c5ba9e517023>
2008-09-19 18:38:07 +0000

    Explicitly give all sub-modules

--- mkdoc.sh
+++ mkdoc.sh
@@ -1,5 +1,5 @@
 #!/bin/sh
 epydoc --html -o html/ --name Papywizard --inheritance listed \
        --url http://trac.gbiloba.org/papywizard --graph all -v \
-       papywizard
-
+       papywizard.common papywizard.controller papywizard.hardware \
+       papywizard.model papywizard.view papywizard.view3D
