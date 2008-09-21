1ec8e9b6430bb09512fc9c02e9678d41e15ca4c6
fma <fma@1bb8f0fc-47fd-4586-b4e1-c5ba9e517023>
2008-09-21 18:33:43 +0000

    Use traceback controller

--- papywiz.py
+++ papywiz.py
@@ -61,6 +61,7 @@ from papywizard.common.serializer import Serializer
 from papywizard.common.exception import HardwareError
 from papywizard.model.shooting import Shooting
 from papywizard.controller.mainController import MainController
+from papywizard.controller.tracebackController import TracebackController
 from papywizard.controller.spy import Spy
 from papywizard.hardware.head import Head, HeadSimulation
 if config.VIEW3D_ENABLE:
@@ -114,8 +115,7 @@ try:
     Logger().info("Papywizard app stopped")
 
 except Exception, msg:
-    messageDialog = gtk.MessageDialog(flags=gtk.DIALOG_MODAL, type=gtk.MESSAGE_ERROR, buttons=gtk.BUTTONS_CLOSE,
-                                      message_format="Internal error")
-    messageDialog.format_secondary_text(str(msg))
-    messageDialog.run()
-    messageDialog.destroy()
+    tracebackDialog = TracebackController()
+    tracebackDialog.run()
+    tracebackDialog.destroyView()
+
