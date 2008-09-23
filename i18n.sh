db5ca41975afa09104b9f855011bab7cd9d2109c
fma <fma@1bb8f0fc-47fd-4586-b4e1-c5ba9e517023>
2008-09-23 12:32:12 +0000

    Updated

--- i18n.sh
+++ i18n.sh
@@ -2,20 +2,28 @@
 
 TMP_DIR="tmp"
 POT_FILE="papywizard.pot"
+MO_FILE="papywizard.mo"
+LOCALE_DIR="locale"
 
 # Extract strings from python files
 python_files="scripts/papywiz.py"
 python_files="$python_files `find papywizard -type f -name \"*.py\"`"
-xgettext --language=Python --from-code=utf-8 --keyword=_ --output=$TMP_DIR/$POT_FILE $python_files
+xgettext --language=Python --from-code=utf-8 --keyword=_ --no-wrap --output=$TMP_DIR/$POT_FILE $python_files
 
 # Extract strings from glade files
 glade_files=`ls papywizard/view/*.glade`
 for file in $glade_files; do
     intltool-extract -l --type=gettext/glade $file
 done
-xgettext --language=Python --from-code=utf-8 --keyword=_ --keyword=N_ --output=$TMP_DIR/$POT_FILE $python_files $TMP_DIR/*.h
+xgettext --language=Python --from-code=utf-8 --keyword=_ --keyword=N_ --no-wrap --output=$TMP_DIR/$POT_FILE $python_files $TMP_DIR/*.h
 
 # Generate PO files
-msginit --input=$TMP_DIR/$POT_FILE --locale=en_US --no-translator --no-wrap --output=$TMP_DIR/en_US.po
-msginit --input=$TMP_DIR/$POT_FILE --locale=fr_FR --no-translator --no-wrap --output=$TMP_DIR/fr_FR.po
-
+for lang in 'en_US' 'fr_FR'; do
+    mkdir -p $LOCALE_DIR/$lang/LC_MESSAGES
+    if [ -e $TMP_DIR/$lang.po ]; then
+        msgmerge -U $TMP_DIR/$lang.po $TMP_DIR/$POT_FILE
+    else
+        msginit --input=$TMP_DIR/$POT_FILE --locale=$lang --no-translator --no-wrap --output=$TMP_DIR/$lang.po
+    fi
+    msgfmt -v --output-file=$LOCALE_DIR/$lang/LC_MESSAGES/$MO_FILE $TMP_DIR/$lang.po
+done
