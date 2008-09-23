60f091b5c0ed7f66e7aa349aaf2991976fa9f465
fma <fma@1bb8f0fc-47fd-4586-b4e1-c5ba9e517023>
2008-09-23 12:33:03 +0000

    Added localization

--- scripts/papywiz.py
+++ scripts/papywiz.py
@@ -51,11 +51,16 @@ import sys
 import os.path
 import traceback
 import StringIO
+import locale
+import gettext
 
 import pygtk
 pygtk.require("2.0")
 import gtk
+import gtk.glade
 import gobject
+
+APP_NAME = "papywizard"
 
 try:
     import papywizard
@@ -74,6 +79,50 @@ from papywizard.controller.spy import Spy
 from papywizard.view.logBuffer import LogBuffer
 if config.VIEW3D_ENABLE:
     from papywizard.view3D.view3D import View3D
+
+
+# Localization (thanks to Mark Mruss from http://www.learningpython.com)
+# Get the local directory since we are not installing anything
+localePath = os.path.realpath(os.path.dirname(sys.argv[0]))
+
+# Init the list of languages to support
+langs = []
+
+# Check the default locale
+lc, encoding = locale.getdefaultlocale()
+if (lc):
+
+    # if we have a default, it's the first in the list
+    langs = [lc]
+
+# Now lets get all of the supported languages on the system
+language = os.environ.get('LANGUAGE', None)
+if (language):
+
+    # Language comes back something like en_CA:en_US:en_GB:en
+    # on linuxy systems, on Win32 it's nothing, so we need to
+    # split it up into a list
+    langs += language.split(":")
+
+# Now add on to the back of the list the translations that we
+# know that we have, our defaults
+langs += ["en_US", "fr_FR"]
+
+# Now langs is a list of all of the languages that we are going
+# to try to use.  First we check the default, then what the system
+# told us, and finally the 'known' list
+
+gettext.bindtextdomain(APP_NAME, localePath)
+gettext.textdomain(APP_NAME)
+gtk.glade.bindtextdomain(APP_NAME, localePath)
+gtk.glade.textdomain(APP_NAME)
+
+# Get the language to use
+lang = gettext.translation(APP_NAME, localePath, languages=langs, fallback=True)
+
+# Install the language, map _()
+global _
+_ = lang.gettext
 
 
 class Papywizard(object):
