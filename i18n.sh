40696634c5d32dd99d9ae9f885fbaa65b0036e49
fma <fma@1bb8f0fc-47fd-4586-b4e1-c5ba9e517023>
2008-09-24 08:43:53 +0000

    Rebuild po/mo file only if modified

--- i18n.sh
+++ i18n.sh
@@ -12,6 +12,7 @@ mkdir -p $TMP_DIR
 # Extract strings from python files
 python_files="scripts/papywiz.py"
 python_files="$python_files `find papywizard -type f -name \"*.py\"`"
+
 xgettext --language=Python --from-code=utf-8 --default-domain=$APP_NAME --keyword=_ --no-wrap --output=$TMP_DIR/$APP_NAME $python_files
 
 # Extract strings from glade files
@@ -19,15 +20,18 @@ glade_files=`ls papywizard/view/*.glade`
 for file in $glade_files; do
     intltool-extract -l --type=gettext/glade $file
 done
+
 xgettext --language=Python --from-code=utf-8 --default-domain=$APP_NAME --keyword=_ --keyword=N_ --no-wrap --output=$TMP_DIR/$POT_FILE $python_files $TMP_DIR/*.h
 
 # Generate PO and MO files
 for lang in 'en' 'fr'; do
-    mkdir -p $LOCALE_DIR/$lang/LC_MESSAGES
-    if [ -e $LOCALE_DIR/$lang/LC_MESSAGES/$PO_FILE ]; then
-        msgmerge -U $LOCALE_DIR/$lang/LC_MESSAGES/$PO_FILE $TMP_DIR/$POT_FILE
-    else
-        msginit --input=$TMP_DIR/$POT_FILE --locale=$lang --no-translator --no-wrap --output=$LOCALE_DIR/$lang/LC_MESSAGES/$PO_FILE
+    if [ $LOCALE_DIR/$lang/LC_MESSAGES/$PO_FILE -nt $LOCALE_DIR/$lang/LC_MESSAGES/$MO_FILE ]; then
+        mkdir -p $LOCALE_DIR/$lang/LC_MESSAGES
+        if [ -e $LOCALE_DIR/$lang/LC_MESSAGES/$PO_FILE ]; then
+            msgmerge -U $LOCALE_DIR/$lang/LC_MESSAGES/$PO_FILE $TMP_DIR/$POT_FILE
+        else
+            msginit --input=$TMP_DIR/$POT_FILE --locale=$lang --no-translator --no-wrap --output=$LOCALE_DIR/$lang/LC_MESSAGES/$PO_FILE
+        fi
+        msgfmt -v --output-file=$LOCALE_DIR/$lang/LC_MESSAGES/$MO_FILE $LOCALE_DIR/$lang/LC_MESSAGES/$PO_FILE
     fi
-    msgfmt -v --output-file=$LOCALE_DIR/$lang/LC_MESSAGES/$MO_FILE $LOCALE_DIR/$lang/LC_MESSAGES/$PO_FILE
 done
