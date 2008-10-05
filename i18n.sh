35ce1d06fac9172914092c35100769c9922a4880
fma <fma@1bb8f0fc-47fd-4586-b4e1-c5ba9e517023>
2008-10-05 15:06:26 +0000

    Added echo

--- i18n.sh
+++ i18n.sh
@@ -12,11 +12,13 @@ mkdir -p $TMP_DIR
 # Extract strings from python files
 python_files="`find papywizard -type f -name \"*.py\"`"
 
+echo xgettext --language=Python --from-code=utf-8 --default-domain=$APP_NAME --keyword=_ --no-wrap --output=$TMP_DIR/$APP_NAME $python_files
 xgettext --language=Python --from-code=utf-8 --default-domain=$APP_NAME --keyword=_ --no-wrap --output=$TMP_DIR/$APP_NAME $python_files
 
 # Extract strings from glade files
 glade_files=`ls papywizard/view/*.glade`
 for file in $glade_files; do
+    echo intltool-extract -l --type=gettext/glade $file
     intltool-extract -l --type=gettext/glade $file
 done
 
@@ -26,10 +28,13 @@ xgettext --language=Python --from-code=utf-8 --default-domain=$APP_NAME --keywor
 for lang in 'en' 'fr' 'pl'; do
     mkdir -p $LOCALE_DIR/$lang/LC_MESSAGES
     if ! [ -e $LOCALE_DIR/$lang/LC_MESSAGES/$PO_FILE ]; then
+        echo msginit --input=$TMP_DIR/$POT_FILE --locale=$lang --no-translator --no-wrap --output=$LOCALE_DIR/$lang/LC_MESSAGES/$PO_FILE
         msginit --input=$TMP_DIR/$POT_FILE --locale=$lang --no-translator --no-wrap --output=$LOCALE_DIR/$lang/LC_MESSAGES/$PO_FILE
     fi
     if [ $TMP_DIR/$POT_FILE -nt $LOCALE_DIR/$lang/LC_MESSAGES/$MO_FILE ]; then
+        echo msgmerge -U $LOCALE_DIR/$lang/LC_MESSAGES/$PO_FILE $TMP_DIR/$POT_FILE
         msgmerge -U $LOCALE_DIR/$lang/LC_MESSAGES/$PO_FILE $TMP_DIR/$POT_FILE
+        echo msgfmt -v --output-file=$LOCALE_DIR/$lang/LC_MESSAGES/$MO_FILE $LOCALE_DIR/$lang/LC_MESSAGES/$PO_FILE
         msgfmt -v --output-file=$LOCALE_DIR/$lang/LC_MESSAGES/$MO_FILE $LOCALE_DIR/$lang/LC_MESSAGES/$PO_FILE
     fi
 done
