8aab0976ebf2540d1999b6262968f6e08f5d1efb
fma <fma@1bb8f0fc-47fd-4586-b4e1-c5ba9e517023>
2008-10-07 15:01:48 +0000

    Updated

--- mkdoc.sh
+++ mkdoc.sh
@@ -1,5 +1,13 @@
 #!/bin/sh
+
+modules="papywizard.common papywizard.controller papywizard.hardware \
+         papywizard.model papywizard.view papywizard.view3D"
+
 epydoc --html -o html/ --name Papywizard --inheritance listed \
        --url http://trac.gbiloba.org/papywizard --graph all -v \
-       papywizard.common papywizard.controller papywizard.hardware \
-       papywizard.model papywizard.view papywizard.view3D
+       $modules
+
+#epydoc --pdf -o papywizard.pdf --name Papywizard --inheritance listed \
+       #--url http://trac.gbiloba.org/papywizard --graph all -v \
+       #$modules
+
