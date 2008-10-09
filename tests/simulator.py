ba1dd6814f0f04657a58ed82a4043e8ee34c41cd
fma <fma@1bb8f0fc-47fd-4586-b4e1-c5ba9e517023>
2008-10-09 07:40:45 +0000

    Socket handler keeps connection open

--- tests/simulator.py
+++ tests/simulator.py
@@ -69,41 +69,51 @@ class MerlinOrionBaseHandler(object):
         """ Init the abstract handler.
         """
         super(MerlinOrionBaseHandler, self).__init__()
-        
+
     def _handleCmd(self, cmd):
         """ Handle a command.
-        
+
         @param cmd: commande to simulate
         @type cmd: str
-        
+
         @return: response
         @rtype: str
-        
+
         raise HardwareError: wrong command
         """
         Logger().debug("MerlinOrionBaseHandler._handleCmd(): cmd=%s" % (repr(cmd)))
-        if cmd[0] != ':':
-            raise HardwareError("Wrong command")
-        
+        if not cmd.startswith(':'):
+            raise HardwareError("Invalid command format")
+
         return "=\r"
+
 
 class MerlinOrionSocketHandler(SocketServer.BaseRequestHandler, MerlinOrionBaseHandler):
     """ Socket-based handler.
     """
     def __init__(self, *args, **kwargs):
         super(MerlinOrionSocketHandler, self).__init__(*args, **kwargs)
-            
+
     def handle(self):
-        Logger().debug("MerlinOrionHandler.handle(): request from ('%s', %d)" % self.client_address)
-        cmd = ""
-        while not cmd.endswith('\r'):
-            data = self.request.recv(1)
-            if not data:
-                raise IOError("Can't read data")
-            cmd += data
-        response = self._handleCmd(cmd)
-        self.request.send(response)
-        #self.request.close()
+        Logger().debug("MerlinOrionHandler.handle(): connection request from ('%s', %d)" % self.client_address)
+        Logger().info("New connection established")
+        while True:
+            cmd = ""
+            while not cmd.endswith('\r'):
+                data = self.request.recv(1)
+                if not data: # connection lost?
+                    Logger().error("MerlinOrionSocketHandler.handle(): can't read data")
+                    break
+                cmd += data
+            if cmd:
+                response = self._handleCmd(cmd)
+                Logger().debug("MerlinOrionHandler.handle(): response=%s" % repr(response))
+                self.request.sendall(response)
+            else:
+                self.request.close()
+                Logger().debug("MerlinOrionHandler.handle(): lost connection with ('%s', %d)" % self.client_address)
+                Logger().info("Connection closed")
+                break
 
 
 class MerlinOrionBaseSimulator(object):
@@ -114,7 +124,7 @@ class MerlinOrionBaseSimulator(object):
         """
         super(MerlinOrionBaseSimulator, self).__init__()
         self._init()
-        
+
     def _init(self):
         """ Init the siumlator IO.
         """
@@ -124,10 +134,12 @@ class MerlinOrionBaseSimulator(object):
         """ Run the simulator.
         """
         raise NotImplementedError
-    
-    
+
+
 class MerlinOrionSocketSimulator(MerlinOrionBaseSimulator):
     """ Socket-based simulator.
+
+    handle_error(request, client_address)
     """
     def _init(self):
         self.__server = SocketServer.TCPServer(("localhost", config.SIMUL_SOCKET_PORT), MerlinOrionSocketHandler)
@@ -162,7 +174,7 @@ class MerlinOrionSerialHandler(object):
                 else:
                     data += c
 
- 
+
 class MerlinOrionSerialSimulator(object):
     """ Serial-based simulator.
     """
@@ -170,10 +182,10 @@ class MerlinOrionSerialSimulator(object):
         self.__serial = serial.Serial(config.DEFAULT_SERIAL_PORT)
         self.__serial.setBaudrate(config.SIMUL_SERIAL_BAUDRATE)
         self.__serial.setTimeout(1.)
-        
+
     def run(self):
         """ Run the sniffer.
-        
+
         Listen to the serial line and display commands/responses.
         """
         Logger().info("Serial-based simulator started")
@@ -203,8 +215,7 @@ class MerlinOrionSerialSimulator(object):
 def main():
     simulator = MerlinOrionSocketSimulator()
     simulator.run()
-    
-    
+
+
 if __name__ == "__main__":
     main()
-    
