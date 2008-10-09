3ceddcf3bff0c7a30c38daf5416a844384a7fa6c
fma <fma@1bb8f0fc-47fd-4586-b4e1-c5ba9e517023>
2008-10-09 13:52:08 +0000

    Added reprStr()

--- papywizard/common/helpers.py
+++ papywizard/common/helpers.py
@@ -50,12 +50,93 @@ Implements
 @author: Frédéric Mantegazza
 @copyright: (C) 2007-2008 Frédéric Mantegazza
 @license: CeCILL
+
+@todo: move Merlin/Orion stuff in hardsare module
 """
 
 __revision__ = "$Id$"
 
+from pprint import PrettyPrinter
+
 from papywizard.common import config
 
+
+def reprStr(obj):
+    """ Return a simple name for the object instance.
+
+    <function myFunc at 0x405b1a3c>                                                     -> myFunc
+    <class 'module1.module2.MyClass'>                                                   -> myClass
+    <class 'module1.module2.MyClass' at 0x408cd3ec>                                     -> MyClass
+    <unbound method MyClass.myMethod>                                                   -> myMethod
+    <bound method MyClass.myMethod of <module1.module2.MyClass instance at 0x40585e0c>> -> myMethod
+    <module1.module2.MyClass instance at 0x40585e2c>                                    -> MyClass at 0x40585e2c
+    <module1.module2.MyClass object at 0x40585e2c>                                      -> MyClass at 0x40585e2c
+    """
+    reprObj = repr(obj)
+    reprObj = reprObj.replace("'", "")
+    reprObj = reprObj.replace("<", "")
+    reprObj = reprObj.replace(">", "")
+
+    if reprObj.find("class") != -1:
+        fields = reprObj.split()
+        strValue = fields[1].split('.')[-1]
+
+    elif reprObj.find("unbound method") != -1:
+        fields = reprObj.split()
+        strValue = fields[2].split('.')[-1]
+
+    elif reprObj.find("bound method") != -1:
+        fields = reprObj.split()
+        strValue = fields[2].split('.')[-1]
+
+    elif reprObj.find("function") != -1:
+        fields = reprObj.split()
+        strValue = fields[1]
+
+    elif reprObj.find("instance") != -1:
+        fields = reprObj.split()
+        strValue = "%s at %s" % (fields[0].split('.')[-1], fields[-1])
+
+    elif reprObj.find("object") != -1:
+        fields = reprObj.split()
+        strValue = "%s at %s" % (fields[0].split('.')[-1], fields[-1])
+
+    else:
+        strValue = reprObj
+
+    #reprList = strValue.split()
+    #for iPos in xrange(len(reprList)):
+
+        ## Default repr for classes
+        #if reprList[iPos] == "<class":
+            #strValue = reprList[iPos+1].split(".")[-1][:-2]
+            #break
+
+        ## Default repr for methods
+        #elif reprList[iPos] == "method":
+            #strValue = reprList[iPos+1]
+            #break
+
+        ## Default repr for instances
+        #elif reprList[iPos] == "object" or reprList[iPos] == "instance":
+            #strValue = reprList[iPos-1].split(".")[-1]+" at "+reprList[iPos+2].split(">")[0]
+            #break
+
+    return strValue
+
+def prettyFormat(value):
+    """ Pretty format a value.
+
+    @param value: value to pretty format
+    @type value: python object
+
+    @return: the value pretty formated
+    @rtype: str
+    """
+    pp = PrettyPrinter()
+    prettyStr = pp.pformat(value)
+
+    return prettyStr
 
 def decodeAxisValue(strValue):
     """ Decode value from axis.
@@ -75,7 +156,6 @@ def decodeAxisValue(strValue):
 
     return value
 
-
 def encodeAxisValue(value):
     """ Encode value for axis.
 
@@ -93,7 +173,6 @@ def encodeAxisValue(value):
 
     return strValue.upper()
 
-
 def cod2deg(codPos):
     """ Convert encoder value to degres.
 
@@ -105,7 +184,6 @@ def cod2deg(codPos):
     """
     return (codPos - config.ENCODER_ZERO) * 360. / config.ENCODER_360
 
-
 def deg2cod(pos):
     """ Convert degres to encoder value.
 
@@ -116,4 +194,3 @@ def deg2cod(pos):
     @rtype: int
     """
     return int(pos * config.ENCODER_360 / 360. + config.ENCODER_ZERO)
-
