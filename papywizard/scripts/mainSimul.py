bfeef2a36ef0208f12ae628f3d951fb11adb684b
fma <fma@1bb8f0fc-47fd-4586-b4e1-c5ba9e517023>
2008-10-14 11:54:58 +0000

    Start with a default connexion

--- papywizard/scripts/mainSimul.py
+++ papywizard/scripts/mainSimul.py
@@ -7,10 +7,10 @@ License
 =======
 
  - B{papywizard} (U{http://trac.gbiloba.org/papywizard}) is Copyright:
-  - (C) 2007-2008 Frédéric Mantegazza
+  - (C) 2007-2008 FrÃ©dÃ©ric Mantegazza
 
 This software is governed by the B{CeCILL} license under French law and
-abiding by the rules of distribution of free software.  You can  use, 
+abiding by the rules of distribution of free software.  You can  use,
 modify and/or redistribute the software under the terms of the CeCILL
 license as circulated by CEA, CNRS and INRIA at the following URL
 U{http://www.cecill.info}.
@@ -19,7 +19,7 @@ As a counterpart to the access to the source code and  rights to copy,
 modify and redistribute granted by the license, users are provided only
 with a limited warranty  and the software's author,  the holder of the
 economic rights,  and the successive licensors  have only  limited
-liability. 
+liability.
 
 In this respect, the user's attention is drawn to the risks associated
 with loading,  using,  modifying and/or developing or reproducing the
@@ -28,9 +28,9 @@ that may mean  that it is complicated to manipulate,  and  that  also
 therefore means  that it is reserved for developers  and  experienced
 professionals having in-depth computer knowledge. Users are therefore
 encouraged to load and test the software's suitability as regards their
-requirements in conditions enabling the security of their systems and/or 
-data to be ensured and,  more generally, to use and operate it in the 
-same conditions as regards security. 
+requirements in conditions enabling the security of their systems and/or
+data to be ensured and,  more generally, to use and operate it in the
+same conditions as regards security.
 
 The fact that you are presently reading this means that you have had
 knowledge of the CeCILL license and that you accept its terms.
@@ -40,8 +40,8 @@ Module purpose
 
 Main script for simulator
 
-@author: Frédéric Mantegazza
-@copyright: (C) 2007-2008 Frédéric Mantegazza
+@author: FrÃ©dÃ©ric Mantegazza
+@copyright: (C) 2007-2008 FrÃ©dÃ©ric Mantegazza
 @license: CeCILL
 """
 
@@ -60,10 +60,11 @@ def main():
     usage = "Usage: %prog bluetooth|serial|ethernet [options]"
     version = "%%prog %s" % config.VERSION
     parser = optparse.OptionParser(usage=usage, version=version)
-    parser.add_option("-c", "--connection", action="store",
-                                            dest="connection",
-                                            choices=('bluetooth', 'serial', 'ethernet'),
-                                            help="Connection")
+    parser.set_defaults(connexion=config.SIMUL_DEFAULT_CONNEXION)
+    parser.add_option("-c", "--connexion", action="store",
+                                           dest="connexion",
+                                           choices=('bluetooth', 'serial', 'ethernet'),
+                                           help="Connection")
     parser.add_option("-a", "--address", action="store",
                                          dest="btAddress",
                                          help="Bluetooth device address")
@@ -88,12 +89,12 @@ def main():
     (option, args) = parser.parse_args()
     #Logger().debug("option=%s" % option)
 
-    if option.connection == 'serial':
+    if option.connexion == 'serial':
         simulator = MerlinOrionSerialSimulator(option.serialPort)
-    elif option.connection == 'bluetooth':
+    elif option.connexion == 'bluetooth':
         Logger().warning("Bluetooth simulator not yet impemented")
         sys.exit(1)
-    elif option.connection == 'ethernet':
+    elif option.connexion == 'ethernet':
         simulator = MerlinOrionEthernetSimulator(option.ethernetHost, option.ethernetPort)
     else:
         parser.print_help()
