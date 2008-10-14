2d784765f75e060af9c570221a6cc687bd69113d
fma <fma@1bb8f0fc-47fd-4586-b4e1-c5ba9e517023>
2008-10-14 14:28:24 +0000

    Generate doc in tmp/ dir

--- mkdoc.sh
+++ mkdoc.sh
@@ -3,11 +3,11 @@
 modules="papywizard.common papywizard.controller papywizard.hardware \
          papywizard.model papywizard.view papywizard.view3D"
 
-epydoc --html -o html/ --name Papywizard --inheritance listed \
+epydoc --html -o tmp/html/ --name Papywizard --inheritance listed \
        --url http://trac.gbiloba.org/papywizard --graph all -v \
        $modules
 
-#epydoc --pdf -o papywizard.pdf --name Papywizard --inheritance listed \
+#epydoc --pdf -o tmp/papywizard.pdf --name Papywizard --inheritance listed \
        #--url http://trac.gbiloba.org/papywizard --graph all -v \
        #$modules
 
