ac3a9c4f8943613b5d2da41bf71a8454f10bb178
fma <fma@1bb8f0fc-47fd-4586-b4e1-c5ba9e517023>
2008-10-14 13:08:54 +0000

    Added logs

--- papywizard/scripts/main.py
+++ papywizard/scripts/main.py
@@ -7,10 +7,10 @@ License
 =======
 
  - B{papywizard} (U{http://trac.gbiloba.org/papywizard}) is Copyright:
-  - (C) 2007-2008 Frédéric Mantegazza
+  - (C) 2007-2008 FrÃ©dÃ©ric Mantegazza
 
 This software is governed by the B{CeCILL} license under French law and
-abiding by the rules of distribution of free software.  You can  use, 
+abiding by the rules of distribution of free software.  You can  use,
 modify and/or redistribute the software under the terms of the CeCILL
 license as circulated by CEA, CNRS and INRIA at the following URL
 U{http://www.cecill.info}.
@@ -19,7 +19,7 @@ As a counterpart to the access to the source code and  rights to copy,
 modify and redistribute granted by the license, users are provided only
 with a limited warranty  and the software's author,  the holder of the
 economic rights,  and the successive licensors  have only  limited
-liability. 
+liability.
 
 In this respect, the user's attention is drawn to the risks associated
 with loading,  using,  modifying and/or developing or reproducing the
@@ -28,9 +28,9 @@ that may mean  that it is complicated to manipulate,  and  that  also
 therefore means  that it is reserved for developers  and  experienced
 professionals having in-depth computer knowledge. Users are therefore
 encouraged to load and test the software's suitability as regards their
-requirements in conditions enabling the security of their systems and/or 
-data to be ensured and,  more generally, to use and operate it in the 
-same conditions as regards security. 
+requirements in conditions enabling the security of their systems and/or
+data to be ensured and,  more generally, to use and operate it in the
+same conditions as regards security.
 
 The fact that you are presently reading this means that you have had
 knowledge of the CeCILL license and that you accept its terms.
@@ -40,8 +40,8 @@ Module purpose
 
 Main script
 
-@author: Frédéric Mantegazza
-@copyright: (C) 2007-2008 Frédéric Mantegazza
+@author: FrÃ©dÃ©ric Mantegazza
+@copyright: (C) 2007-2008 FrÃ©dÃ©ric Mantegazza
 @license: CeCILL
 """
 
@@ -83,7 +83,7 @@ class BlackHole:
     """
     softspace = 0
 
-    def write(self, text): 
+    def write(self, text):
         pass
 
 
@@ -136,7 +136,7 @@ class Papywizard(object):
         except ImportError:
             Logger().warning("Lobilab aspects module must be installed to use logging aspects")
 
-    def i10n(self):
+    def l10n(self):
         """ i10n stuff.
 
         Thanks to Mark Mruss from http://www.learningpython.com
@@ -147,6 +147,7 @@ class Papywizard(object):
 
         # Check the default locale
         lc, encoding = locale.getdefaultlocale()
+        Logger().debug("Papywizard.l10n(): default locale=%s" % lc)
         if lc:
 
             # If we have a default, it's the first in the list
@@ -154,6 +155,7 @@ class Papywizard(object):
 
         # Now lets get all of the supported languages on the system
         language = os.environ.get('LANGUAGE', None)
+        Logger().debug("Papywizard.l10n(): LANGUAGE=%s" % language)
         if language is not None:
 
             # Language comes back something like en_CA:en_US:en_GB:en
@@ -169,7 +171,7 @@ class Papywizard(object):
             if lang not in langs:
                 langs.append(lang)
 
-        Logger().debug("Papywizard.in10n(): langs=%s" % langs)
+        Logger().debug("Papywizard.l10n(): langs=%s" % langs)
 
         # Get the locale dir
         # First check in windows install
@@ -190,8 +192,10 @@ class Papywizard(object):
 
                 if localeFile is not None:
                     localeDir = os.path.join(os.path.dirname(localeFile), os.pardir, os.pardir)
-        Logger().debug("Papywizard.in10n(): localeDir=%s" % localeDir)
+        Logger().debug("Papywizard.l10n(): localeDir=%s" % localeDir)
 
+        #gettext.bindtextdomain(DOMAIN, localeDir)
+        #gettext.textdomain(DOMAIN)
         gtk.glade.bindtextdomain(DOMAIN, localeDir)
         gtk.glade.textdomain(DOMAIN)
 
@@ -234,7 +238,7 @@ def main():
 
     try:
         app = Papywizard()
-        app.i10n()
+        app.l10n()
         app.init()
         #app.weave()
         try:
