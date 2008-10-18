8ad5eba68610bfcf34e843aa3d5876ea64de70cf
fma <fma@1bb8f0fc-47fd-4586-b4e1-c5ba9e517023>
2008-10-18 07:05:25 +0000

    Remove sources Capitalized names

--- windows/setup.py
+++ windows/setup.py
@@ -9,7 +9,7 @@ License
   - (C) 2007-2008 Frédéric Mantegazza
 
 This software is governed by the B{CeCILL} license under French law and
-abiding by the rules of distribution of free software.  You can  use, 
+abiding by the rules of distribution of free software.  You can  use,
 modify and/or redistribute the software under the terms of the CeCILL
 license as circulated by CEA, CNRS and INRIA at the following URL
 U{http://www.cecill.info}.
@@ -18,7 +18,7 @@ As a counterpart to the access to the source code and  rights to copy,
 modify and redistribute granted by the license, users are provided only
 with a limited warranty  and the software's author,  the holder of the
 economic rights,  and the successive licensors  have only  limited
-liability. 
+liability.
 
 In this respect, the user's attention is drawn to the risks associated
 with loading,  using,  modifying and/or developing or reproducing the
@@ -27,9 +27,9 @@ that may mean  that it is complicated to manipulate,  and  that  also
 therefore means  that it is reserved for developers  and  experienced
 professionals having in-depth computer knowledge. Users are therefore
 encouraged to load and test the software's suitability as regards their
-requirements in conditions enabling the security of their systems and/or 
-data to be ensured and,  more generally, to use and operate it in the 
-same conditions as regards security. 
+requirements in conditions enabling the security of their systems and/or
+data to be ensured and,  more generally, to use and operate it in the
+same conditions as regards security.
 
 The fact that you are presently reading this means that you have had
 knowledge of the CeCILL license and that you accept its terms.
@@ -99,15 +99,15 @@ setup(options={"py2exe": {'compressed': 1,
       windows=[Target(description="Merlin/Orion panohead control software",
                       script="../papywizard/scripts/main.py",
                       icon_resources=[(1, "papywizard.ico")],
-                      dest_base="papywizard"),
-               Target(description="Merlin/Orion panohead control software",
+                      dest_base="Papywizard"),
+               Target(description="Papywizard 3D view",
                       script="../papywizard/scripts/main3D.py",
                       icon_resources=[(1, "papywizard.ico")],
-                      dest_base="papywizard-3D")],
-      console=[Target(description="Merlin/Orion panohead control software",
+                      dest_base="Papywizard-3D")],
+      console=[Target(description="Papywizard harware simulator",
                       script="../papywizard/scripts/mainSimul.py",
                       icon_resources=[(1, "papywizard.ico")],
-                      dest_base="papywizard-simul")],
+                      dest_base="Papywizard-Simul")],
       data_files=[("papywizard/common", ["../papywizard/common/papywizard.conf",
                                          "../papywizard/common/presets.xml"]),
                   ("papywizard/view", ["../papywizard/view/papywizard.png",
