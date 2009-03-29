8dc11c687403aa937c357b73bf1eab71fecd50c2
fma <fma@1bb8f0fc-47fd-4586-b4e1-c5ba9e517023>
2009-03-29 18:07:15 +0000

    First commit

---
+++ papywizard/hardware/shutterPlugin.py
@@ -0,0 +1,130 @@
+# -*- coding: utf-8 -*-
+
+""" Panohead remote control.
+
+License
+=======
+
+ - B{papywizard} (U{http://trac.gbiloba.org/papywizard}) is Copyright:
+  - (C) 2007-2009 Frédéric Mantegazza
+
+This software is governed by the B{CeCILL} license under French law and
+abiding by the rules of distribution of free software.  You can  use, 
+modify and/or redistribute the software under the terms of the CeCILL
+license as circulated by CEA, CNRS and INRIA at the following URL
+U{http://www.cecill.info}.
+
+As a counterpart to the access to the source code and  rights to copy,
+modify and redistribute granted by the license, users are provided only
+with a limited warranty  and the software's author,  the holder of the
+economic rights,  and the successive licensors  have only  limited
+liability. 
+
+In this respect, the user's attention is drawn to the risks associated
+with loading,  using,  modifying and/or developing or reproducing the
+software by the user in light of its specific status of free software,
+that may mean  that it is complicated to manipulate,  and  that  also
+therefore means  that it is reserved for developers  and  experienced
+professionals having in-depth computer knowledge. Users are therefore
+encouraged to load and test the software's suitability as regards their
+requirements in conditions enabling the security of their systems and/or 
+data to be ensured and,  more generally, to use and operate it in the 
+same conditions as regards security. 
+
+The fact that you are presently reading this means that you have had
+knowledge of the CeCILL license and that you accept its terms.
+
+Module purpose
+==============
+
+Plugin
+
+Implements
+==========
+
+- ShutterPlugin
+
+@author: Frédéric Mantegazza
+@copyright: (C) 2007-2009 Frédéric Mantegazza
+@license: CeCILL
+"""
+
+__revision__ = "$Id: axisPlugin.py 1595 2009-03-12 12:39:38Z fma $"
+
+from papywizard.common.abstractPlugin import AbstractPlugin
+
+
+class ShutterPlugin(AbstractPlugin):
+    capacity = "shutter"
+
+    def _init(self):
+        pass
+
+    def _getTimeValue(self):
+        """
+        """
+        raise NotImplementedError("ShutterPlugin._getTimeValue() must be overloaded")
+
+    def __getTimeValue(self):
+        return self._getTimeValue()
+
+    timeValue = property(__getTimeValue)
+
+    def _getMirrorLockup(self):
+        """
+        """
+        raise NotImplementedError("ShutterPlugin._getMirrorLockup() must be overloaded")
+
+    def __getMirrorLockup(self):
+        return self._getMirrorLockup()
+
+    mirrorLockup = property(__getMirrorLockup)
+
+    def _getBracketingNbPicts(self):
+        """
+        """
+        raise NotImplementedError("ShutterPlugin._getBracketingNbPicts() must be overloaded")
+
+    def __getBracketingNbPicts(self):
+        return self._getBracketingNbPicts()
+
+    bracketingNbPicts = property(__getBracketingNbPicts)
+
+    def _getBracketingIntent(self):
+        """
+        """
+        raise NotImplementedError("ShutterPlugin.__getBracketingIntent() must be overloaded")
+
+    def __getBracketingIntent(self):
+        return self._getBracketingIntent()
+
+    bracketingIntent = property(__getBracketingIntent)
+
+    def _defineConfig(self):
+        pass
+        #HardwarePlugin._defineConfig(self)
+        #self._addConfigKey('_timeValue', 'TIME_VALUE', default=0.5)
+        #self._addConfigKey('_mirrorLockup', 'MIRROR_LOCKUP', default=False)
+        #self._addConfigKey('_pulseWidthHigh', 'PULSE_WIDTH_HIGH', default=100)
+        #self._addConfigKey('_pulseWidthLow', 'PULSE_WIDTH_LOW', default=100)
+        #self._addConfigKey('_bracketingNbPicts', 'BRACKETING_NB_PICTS', default=1)
+        #self._addConfigKey('_bracketingIntent', 'BRACKETING_INTENT', default='exposure')
+
+    def lockupMirror(self):
+        """ Lockup the mirror.
+
+        @return: error code (0 for OK)
+        @rtype: int
+        """
+        raise NotImplementedError("ShutterPlugin.lockupMirror() must be overloaded")
+
+    def shoot(self, bracketNumber):
+        """ Shoot.
+
+        @param bracketNumber: number of the bracketed picture
+        @type bracketNumber: int
+
+        @return: error code (0 for OK)
+        @rtype: int
+        """
+        raise NotImplementedError("ShutterPlugin.shoot() must be overloaded")
