0cbbd11009ef98ffbb3ab34fc7687d38dea9d564
fma <fma@1bb8f0fc-47fd-4586-b4e1-c5ba9e517023>
2009-03-29 18:06:59 +0000

    First commit

---
+++ papywizard/plugins/tetheredPlugins.py
@@ -0,0 +1,136 @@
+# -*- coding: utf-8 -*-
+
+""" Panohead remote control.
+
+License
+=======
+
+ - B{papywizard} (U{http://trac.gbiloba.org/papywizard}) is Copyright:
+  - (C) 2007-2009 Frédéric Mantegazza
+
+This software is governed by the B{CeCILL} license under French law and
+abiding by the rules of distribution of free software.  You can  use, 
+modify and/or redistribute the software under the terms of the CeCILL
+license as circulated by CEA, CNRS and INRIA at the following URL
+U{http://www.cecill.info}.
+
+As a counterpart to the access to the source code and  rights to copy,
+modify and redistribute granted by the license, users are provided only
+with a limited warranty  and the software's author,  the holder of the
+economic rights,  and the successive licensors  have only  limited
+liability. 
+
+In this respect, the user's attention is drawn to the risks associated
+with loading,  using,  modifying and/or developing or reproducing the
+software by the user in light of its specific status of free software,
+that may mean  that it is complicated to manipulate,  and  that  also
+therefore means  that it is reserved for developers  and  experienced
+professionals having in-depth computer knowledge. Users are therefore
+encouraged to load and test the software's suitability as regards their
+requirements in conditions enabling the security of their systems and/or 
+data to be ensured and,  more generally, to use and operate it in the 
+same conditions as regards security. 
+
+The fact that you are presently reading this means that you have had
+knowledge of the CeCILL license and that you accept its terms.
+
+Module purpose
+==============
+
+Plugins
+
+Implements
+==========
+
+- TetheredShutter
+- TetheredShutterController
+
+@author: Frédéric Mantegazza
+@copyright: (C) 2007-2009 Frédéric Mantegazza
+@license: CeCILL
+"""
+
+__revision__ = "$Id: axisPlugin.py 1595 2009-03-12 12:39:38Z fma $"
+
+import time
+import subprocess
+
+from PyQt4 import QtCore
+
+from papywizard.common import config
+from papywizard.common.loggingServices import Logger
+from papywizard.common.pluginManager import PluginManager
+from papywizard.hardware.shutterPlugin import ShutterPlugin
+from papywizard.controller.shutterPluginController import ShutterPluginController
+from papywizard.view.pluginFields import ComboBoxField, LineEditField, SpinBoxField, DoubleSpinBoxField, CheckBoxField, SliderField
+
+
+class TetheredShutter(ShutterPlugin):
+    name = "Tethered"
+
+    def _init(self):
+        pass
+
+    def _getTimeValue(self):
+        return -1
+
+    def _getMirrorLockup(self):
+        print self._config['MIRROR_LOCKUP']
+        return self._config['MIRROR_LOCKUP']
+
+    def _getBracketingNbPicts(self):
+        return self._config['BRACKETING_NB_PICTS']
+
+    def _getBracketingIntent(self):
+        return self._config['BRACKETING_INTENT']
+
+    def _defineConfig(self):
+        ShutterPlugin._defineConfig(self)
+        self._addConfigKey('_mirrorLockup', 'MIRROR_LOCKUP', default=False)
+        self._addConfigKey('_mirrorLockupCommand', 'MIRROR_LOCKUP_COMMAND', default="")
+        self._addConfigKey('_shootCommand', 'SHOOT_COMMAND', default="")
+        self._addConfigKey('_bracketingNbPicts', 'BRACKETING_NB_PICTS', default=1)
+        self._addConfigKey('_bracketingIntent', 'BRACKETING_INTENT', default='exposure')
+
+    def activate(self):
+        Logger().trace("TetheredShutter.activate()")
+
+    def shutdown(self):
+        Logger().trace("TetheredShutter.shutdown()")
+
+    def lockupMirror(self):
+        Logger().debug("TetheredShutter.lockupMirror(): execute command '%s'..." % self._config['MIRROR_LOCKUP_COMMAND'])
+        time.sleep(1)
+        Logger().debug("TetheredShutter.lockupMirror(): command over")
+        return 0
+
+    def shoot(self, bracketNumber):
+        Logger().debug("TetheredShutter.shoot(): execute command '%s'..." % self._config['SHOOT_COMMAND'])
+
+        # Launch external command
+        args = self._config['SHOOT_COMMAND'].split()
+        p = subprocess.Popen(args, stdout=subprocess.PIPE, stderr=subprocess.PIPE)
+    
+        # Wait end of execution
+        stdout, stderr = p.communicate()
+        if stderr:
+            Logger().debug("TetheredShutter.shoot(): externalShooting.shoot() stderr:\n%s" % stderr)
+        Logger().debug("TetheredShutter.shoot(): externalShooting.shoot() stdout:\n%s" % stdout)
+
+        return p.returncode
+
+
+class TetheredShutterController(ShutterPluginController):
+    def _defineGui(self):
+        ShutterPluginController._defineGui(self)
+        self._addWidget('Main', "Mirror lockup", CheckBoxField, (), 'MIRROR_LOCKUP')
+        self._addWidget('Main', "Mirror lockup command", LineEditField, (), 'MIRROR_LOCKUP_COMMAND')
+        self._addWidget('Main', "Shoot command", LineEditField, (), 'SHOOT_COMMAND')
+        self._addWidget('Main', "Bracketing nb picts", SpinBoxField, (1, 99), 'BRACKETING_NB_PICTS')
+        self._addWidget('Main', "Bracketing intent", ComboBoxField, (['exposure', 'focus', 'white balance', 'movement'],), 'BRACKETING_INTENT')
+
+
+def register():
+    """ Register plugins.
+    """
+    PluginManager().register(TetheredShutter, TetheredShutterController)
