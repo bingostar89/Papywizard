b5e4e50b8a000516456f89ca2aacec3f310c6442
fma <fma@1bb8f0fc-47fd-4586-b4e1-c5ba9e517023>
2009-05-04 09:11:22 +0000

    Moved Merlin functions to plugin

--- papywizard/common/helpers.py
+++ papywizard/common/helpers.py
@@ -42,10 +42,6 @@ Helper functions
 Implements
 ==========
 
-- decodeAxisValue
-- encodeAxisValue
-- cod2deg
-- deg2cod
 - hmsToS
 - hmsToS
 - hmsAsStrToS
@@ -150,71 +146,6 @@ def isOdd(value):
     @type value: int
     """
     return int(value / 2.) != value / 2.
-
-def decodeAxisValue(strValue):
-    """ Decode value from axis.
-
-    Values (position, speed...) returned by axis are
-    32bits-encoded strings, low byte first.
-
-    @param strValue: value returned by axis
-    @type strValue: str
-
-    @return: value
-    @rtype: int
-
-    @todo: put in merlinHelpers?
-    """
-    value = 0
-    for i in xrange(3):
-        value += eval("0x%s" % strValue[i*2:i*2+2]) * 2 ** (i * 8)
-
-    return value
-
-def encodeAxisValue(value):
-    """ Encode value for axis.
-
-    Values (position, speed...) to send to axis must be
-    32bits-encoded strings, low byte first.
-
-    @param value: value
-    @type value: int
-
-    @return: value to send to axis
-    @rtype: str
-
-    @todo: put in merlinHelpers?
-    """
-    strHexValue = "000000%s" % hex(value)[2:]
-    strValue = strHexValue[-2:] + strHexValue[-4:-2] + strHexValue[-6:-4]
-
-    return strValue.upper()
-
-def cod2deg(codPos):
-    """ Convert encoder value to degres.
-
-    @param codPos: encoder position
-    @type codPos: int
-
-    @return: position, in °
-    @rtype: float
-
-    @todo: put in merlinHelpers?
-    """
-    return (codPos - config.ENCODER_ZERO) * 360. / config.ENCODER_360
-
-def deg2cod(pos):
-    """ Convert degres to encoder value.
-
-    @param pos: position, in °
-    @type pos: float
-
-    @return: encoder position
-    @rtype: int
-
-    @todo: put in merlinHelpers?
-    """
-    return int(pos * config.ENCODER_360 / 360. + config.ENCODER_ZERO)
 
 def hmsToS(h, m, s):
     """ Convert hours, minutes, seconds to seconds.
