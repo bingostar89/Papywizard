ec27ea2ecd62ab2d5ddc229d29d90c70f099ea69
fma <fma@1bb8f0fc-47fd-4586-b4e1-c5ba9e517023>
2009-08-28 12:09:44 +0000

    Use QPen for setPen() method

--- papywizard/view/pictureItem.py
+++ papywizard/view/pictureItem.py
@@ -9,7 +9,7 @@ License
   - (C) 2007-2009 Frédéric Mantegazza
 
 This software is governed by the B{CeCILL} license under French law and
-abiding by the rules of distribution of free software.  You can  use, 
+abiding by the rules of distribution of free software.  You can  use,
 modify and/or redistribute the software under the terms of the CeCILL
 license as circulated by CEA, CNRS and INRIA at the following URL
 U{http://www.cecill.info}.
@@ -18,7 +18,7 @@ As a counterpart to the access to the source code and  rights to copy,
 modify and redistribute granted by the license, users are provided only
 with a limited warranty  and the software's author,  the holder of the
 economic rights,  and the successive licensors  have only  limited
-liability. 
+liability.
 
 In this respect, the user's attention is drawn to the risks associated
 with loading,  using,  modifying and/or developing or reproducing the
@@ -27,9 +27,9 @@ that may mean  that it is complicated to manipulate,  and  that  also
 therefore means  that it is reserved for developers  and  experienced
 professionals having in-depth computer knowledge. Users are therefore
 encouraged to load and test the software's suitability as regards their
-requirements in conditions enabling the security of their systems and/or 
-data to be ensured and,  more generally, to use and operate it in the 
-same conditions as regards security. 
+requirements in conditions enabling the security of their systems and/or
+data to be ensured and,  more generally, to use and operate it in the
+same conditions as regards security.
 
 The fact that you are presently reading this means that you have had
 knowledge of the CeCILL license and that you accept its terms.
@@ -185,14 +185,14 @@ class CrosshairCusrsor(QtGui.QGraphicsItemGroup):
 
         self._yawLine = QtGui.QGraphicsLineItem()
         self._yawLine.setLine(0, -size, 0, size)
-        self._yawLine.setPen(QtGui.QColor(*config.SHOOTING_COLOR_SCHEME[config.COLOR_SCHEME]['head']))
+        self._yawLine.setPen(QtGui.QPen(QtGui.QColor(*config.SHOOTING_COLOR_SCHEME[config.COLOR_SCHEME]['head'])))
         self.addToGroup(self._yawLine)
         self._pitchLine = QtGui.QGraphicsLineItem()
         self._pitchLine.setLine(-size, 0, size, 0)
-        self._pitchLine.setPen(QtGui.QColor(*config.SHOOTING_COLOR_SCHEME[config.COLOR_SCHEME]['head']))
+        self._pitchLine.setPen(QtGui.QPen(QtGui.QColor(*config.SHOOTING_COLOR_SCHEME[config.COLOR_SCHEME]['head'])))
         self.addToGroup(self._pitchLine)
         self._circle = QtGui.QGraphicsEllipseItem(-size / 2, -size / 2, size, size)
-        self._circle.setPen(QtGui.QColor(*config.SHOOTING_COLOR_SCHEME[config.COLOR_SCHEME]['head']))
+        self._circle.setPen(QtGui.QPen(QtGui.QColor(*config.SHOOTING_COLOR_SCHEME[config.COLOR_SCHEME]['head'])))
         self.addToGroup(self._circle)
         #self.rotate(45)
 
