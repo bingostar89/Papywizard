ab529cc851b5326ef8dbbb842474b08d8757396c
fma <fma@1bb8f0fc-47fd-4586-b4e1-c5ba9e517023>
2009-09-16 13:10:49 +0000

    Switched status to icons

--- papywizard/controller/pluginsStatusController.py
+++ papywizard/controller/pluginsStatusController.py
@@ -56,14 +56,12 @@ from PyQt4 import QtCore, QtGui
 from papywizard.common.loggingServices import Logger
 from papywizard.controller.abstractController import AbstractModalDialogController
 
-TEXT_STATUS = {True: "Ok", False: "Failed"}
-
 
 class PluginsStatusController(AbstractModalDialogController):
     """ Plugins status controller object.
     """
     def _init(self):
-        self._uiFile = "pluginsStatusDialog.ui"
+        self._uiFile = "pluginsStatusDialog2.ui"
 
     def _initWidgets(self):
         pass
@@ -78,17 +76,18 @@ class PluginsStatusController(AbstractModalDialogController):
         @param status: status of the plugin
         @type status: bool
         """
-        widget.setText(TEXT_STATUS[status])
-        #if status:
-            #widget.
+        if status:
+            widget.setPixmap(QtGui.QPixmap(":/icons/button_ok.png").scaled(16, 16))
+        else:
+            widget.setPixmap(QtGui.QPixmap(":/icons/button_cancel.png").scaled(16, 16))
 
     # Callbacks
 
     # Interface
     def refreshView(self):
-        self.__setStatus(self._view.yawStartStopLineEdit, self._model['yawAxis']['connect'])
-        self.__setStatus(self._view.yawInitShutdownLineEdit, self._model['yawAxis']['init'])
-        self.__setStatus(self._view.pitchStartStopLineEdit, self._model['pitchAxis']['connect'])
-        self.__setStatus(self._view.pitchInitShutdownLineEdit, self._model['pitchAxis']['init'])
-        self.__setStatus(self._view.shutterStartStopLineEdit, self._model['shutter']['connect'])
-        self.__setStatus(self._view.shutterInitShutdownLineEdit, self._model['shutter']['init'])
+        self.__setStatus(self._view.yawStartStopLabel, self._model['yawAxis']['connect'])
+        self.__setStatus(self._view.yawInitShutdownLabel, self._model['yawAxis']['init'])
+        self.__setStatus(self._view.pitchStartStopLabel, self._model['pitchAxis']['connect'])
+        self.__setStatus(self._view.pitchInitShutdownLabel, self._model['pitchAxis']['init'])
+        self.__setStatus(self._view.shutterStartStopLabel, self._model['shutter']['connect'])
+        self.__setStatus(self._view.shutterInitShutdownLabel, self._model['shutter']['init'])
