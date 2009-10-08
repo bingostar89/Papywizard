59f5d7c34a2c2da3def1703340ac50ce0ee9a484
fma <fma@1bb8f0fc-47fd-4586-b4e1-c5ba9e517023>
2009-10-08 11:44:26 +0000

    Change web site link

--- mkdoc.sh
+++ mkdoc.sh
@@ -5,7 +5,7 @@ modules="papywizard.common papywizard.controller papywizard.hardware \
          papywizard.model papywizard.view papywizard.plugins"
 
 epydoc --html -o api/ --name Papywizard --inheritance listed \
-       --url http://trac.gbiloba.org/papywizard --graph all -v \
+       --url http://www.papywizard.org --graph all -v \
        --exclude papywizard.common.i18n \
        --exclude papywizard.common.pixmaps \
        --exclude papywizard.view.icons \
