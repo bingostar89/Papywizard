0a177e51703f2ee4ffcf8fa5f2ec50b07195ae08
fma <fma@1bb8f0fc-47fd-4586-b4e1-c5ba9e517023>
2010-01-10 18:53:49 +0000

    Updated version

--- papywizard/common/config.py
+++ papywizard/common/config.py
@@ -52,7 +52,7 @@ import os.path
 # Version
 VERSION_MAJOR = 2
 VERSION_MINOR = 1 # Odd means dev. release
-VERSION_UPDATE = 13
+VERSION_UPDATE = 14
 VERSION = "%d.%d.%d" % (VERSION_MAJOR, VERSION_MINOR, VERSION_UPDATE)
 VERSION_XML = "b"
 
