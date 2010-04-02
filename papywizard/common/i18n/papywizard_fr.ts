c64e0f6644324552c9205b15ce2507f3602046ed
fma <fma@1bb8f0fc-47fd-4586-b4e1-c5ba9e517023>
2010-04-02 11:35:18 +0000

    Updated

--- papywizard/common/i18n/papywizard_fr.ts
+++ papywizard/common/i18n/papywizard_fr.ts
@@ -1,6 +1,6 @@
 <?xml version="1.0" encoding="utf-8"?>
-<!DOCTYPE TS><TS version="1.1" language="fr">
-<defaultcodec></defaultcodec>
+<!DOCTYPE TS>
+<TS version="2.0" language="fr">
 <context>
     <name>AbortMessageDialog</name>
     <message>
@@ -458,7 +458,7 @@ exposition appareil</translation>
     </message>
     <message>
         <source>Bulb</source>
-        <translation type="unfinished">Pause</translation>
+        <translation>Pause</translation>
     </message>
     <message>
         <source>Enable</source>
