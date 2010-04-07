8cc078ac2306d557d926bfb7ddda0e0209e51360
fma <fma@1bb8f0fc-47fd-4586-b4e1-c5ba9e517023>
2010-04-07 10:33:56 +0000

    New data file version

--- papywizard/common/config.py
+++ papywizard/common/config.py
@@ -54,7 +54,7 @@ VERSION_MAJOR = 2
 VERSION_MINOR = 1  # Odd means dev. release
 VERSION_UPDATE = 17
 VERSION = "%d.%d.%d" % (VERSION_MAJOR, VERSION_MINOR, VERSION_UPDATE)
-VERSION_XML = "b"
+VERSION_XML = "c"
 
 # Paths
 HOME_DIR = os.path.expanduser("~")
