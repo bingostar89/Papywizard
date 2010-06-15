c371ae54ffa1baff9da87385171567be23888ff1
fma <fma@1bb8f0fc-47fd-4586-b4e1-c5ba9e517023>
2010-06-15 05:46:31 +0000

    Improved

--- papywizard/plugins/ursaMinorBt2Plugins.py
+++ papywizard/plugins/ursaMinorBt2Plugins.py
@@ -39,6 +39,30 @@ Module purpose
 
 Hardware
 
+All of the two contacts of the jack socket are implemented and driven through two optocouplers.
+There is a command "X" to drive them. Command format: :X1n  where n can be 0,1,2 or 3.
+The two bits represents the two contacts.
+The state of the two contact can be read by the ':x1' command.
+
+The Bluetooth interface also has an internal millisecond resolution timer to produce exact timing for shoot or AF trigger.
+The internal timer can be programmed in 1ms - 65sec range. Hopefully it is enough for HDR.
+
+The timer can be controlled by this command:
+:T1abnnnn
+
+'a' is the state of the two contacts before timer period.
+'b' is the state of the two contacts after the timer period expires.
+'nnnn' is a hexadecimal value representing range 0000 - ffff in milliseconds.
+
+As you send the T command, the contacts were immediatelly set to 'a'. The timer starts counting, when it expires,
+the contats will be set to 'b'.
+
+The operation of the timer is asynchronous.
+During the timer period, the unit remains active, it operates noramally, forwards all commands to Merlin.
+
+The state of the timer can be monitored by the ':t1' command.
+The format of the response is '=nnnn'.
+
 Implements
 ==========
 
@@ -58,11 +82,13 @@ from PyQt4 import QtCore, QtGui
 
 from papywizard.common import config
 from papywizard.common.loggingServices import Logger
+#from papywizard.hardware.ursaMinorBt2Hardware import UrsaMinorBt2Hardware
+from papywizard.hardware.abstractHardware import AbstractHardware
 from papywizard.plugins.pluginsManager  import PluginsManager
 from papywizard.plugins.abstractHardwarePlugin import AbstractHardwarePlugin
-from papywizard.plugins.abstractStandardShutterPlugin import AbstractStandardShutterPlugin
+from papywizard.plugins.shutterPlugin import ShutterPlugin
 from papywizard.plugins.hardwarePluginController import HardwarePluginController
-from papywizard.plugins.standardShutterPluginController import StandardShutterPluginController
+from papywizard.plugins.shutterPluginController import ShutterPluginController
 from papywizard.view.pluginFields import ComboBoxField, LineEditField, SpinBoxField, DoubleSpinBoxField, CheckBoxField, SliderField
 
 NAME = "Ursa Minor BT2"
@@ -71,16 +97,18 @@ DEFAULT_FOCUS_ENABLE = False
 DEFAULT_FOCUS_PULSE_WIDTH = 0.5  # (s)
 DEFAULT_FOCUS_MAINTAIN = False
 
+DEFAULT_INTERNAL_TIMER_ENABLE = False
+
 TAB_FOCUS = unicode(QtGui.QApplication.translate("ursaMinorBt2Plugins", 'Focus'))
 LABEL_FOCUS_ENABLE = unicode(QtGui.QApplication.translate("ursaMinorBt2Plugins", "Enable"))
-LABEL_PULSE_WIDTH = unicode(QtGui.QApplication.translate("ursaMinorBt2Plugins", "Pulse width"))
-LABEL_MAINTAIN_FOCUS = unicode(QtGui.QApplication.translate("ursaMinorBt2Plugins", "Maintain focus"))
+LABEL_FOCUS_PULSE_WIDTH = unicode(QtGui.QApplication.translate("ursaMinorBt2Plugins", "Pulse width"))
+LABEL_MAINTAIN_FOCUS = unicode(QtGui.QApplication.translate("ursaMinorBt2Plugins", "Maintain with shutter"))
 
 
-class UrsaMinorBt2ShutterHardware(AbstractHardwarePlugin):
+class UrsaMinorBt2Hardware(AbstractHardware):
     """
     """
-    def _sendCmd(self, cmd, param=""):
+    def __sendCmd(self, cmd, param=""):
         """ Send a command to the axis.
 
         @param cmd: command to send
@@ -109,29 +137,52 @@ class UrsaMinorBt2ShutterHardware(AbstractHardwarePlugin):
                     answer += c
 
             except IOError:
-                Logger().exception("UrsaMinorBt2Shutter._sendCmd")
-                Logger().warning("UrsaMinorBt2Shutter._sendCmd(): can't sent command '%s'. Retrying..." % cmd)
+                Logger().exception("UrsaMinorBt2Shutter.__sendCmd")
+                Logger().warning("UrsaMinorBt2Shutter.__sendCmd(): can't sent command '%s'. Retrying..." % cmd)
             else:
                 break
         else:
             raise HardwareError("%s can't send command '%s'" % (NAME, cmd))
-        #Logger().debug("UrsaMinorBt2Shutter._sendCmd(): cmd=%s, ans=%s" % (cmd, answer))
+        #Logger().debug("UrsaMinorBt2Shutter.__sendCmd(): cmd=%s, ans=%s" % (cmd, answer))
 
         return answer
 
+    def setOutput(self, focus, shutter):
+        """ Set outputs state.
 
-class UrsaMinorBt2Shutter(UrsaMinorBt2ShutterHardware, AbstractStandardShutterPlugin):  # Use a AbstractEnhancedShutterPlugin
+        @param focus: new state of the the AF output
+        @type focus: bool
+
+        @param shutter: new state of the shutter output
+        @type shutter: bool
+        """
+        value = int(focus) << 1 + int(shutter)
+        self._driver.acquireBus()
+        try:
+            self.___sendCmd("X", str(value))
+        finally:
+            self._driver.releaseBus()
+
+
+class UrsaMinorBt2Shutter(AbstractHardwarePlugin, ShutterPlugin):
     """
     """
+    def __init__(self, *args, **kwargs):
+        """
+        """
+        AbstractHardwarePlugin.__init__(self, *args, **kwargs)
+        ShutterPlugin.__init__(self, *args, **kwargs)
+
     def _init(self):
         Logger().trace("UrsaMinorBt2Shutter._init()")
         AbstractHardwarePlugin._init(self)
-        AbstractStandardShutterPlugin._init(self)
+        ShutterPlugin._init(self)
+        self._hardware = UrsaMinorBt2Hardware()
 
     def _defineConfig(self):
         Logger().trace("UrsaMinorBt2Shutter._defineConfig()")
         AbstractHardwarePlugin._defineConfig(self)
-        AbstractStandardShutterPlugin._defineConfig(self)
+        ShutterPlugin._defineConfig(self)
         self._addConfigKey('_focus', 'FOCUS_ENABLE', default=DEFAULT_FOCUS_ENABLE)
         self._addConfigKey('_focusPulse', 'FOCUS_PULSE_WIDTH', default=DEFAULT_FOCUS_PULSE_WIDTH)
         self._addConfigKey('_maintainFocus', 'FOCUS_MAINTAIN', default=DEFAULT_FOCUS_MAINTAIN)
@@ -139,27 +190,36 @@ class UrsaMinorBt2Shutter(UrsaMinorBt2ShutterHardware, AbstractStandardShutterPl
     def _triggerOnShutter(self):
         """ Set the shutter on.
         """
-        self._sendCmd("X", "1")
+        self.__sendCmd("X", "1")
 
     def _triggerOffShutter(self):
         """ Set the shutter off.
         """
-        self._sendCmd("X", "0")
+        self.__sendCmd("X", "0")
 
     def shutdown(self):
         Logger().trace("UrsaMinorBt2Shutter.shutdown()")
         self._triggerOffShutter()
 
 
-class UrsaMinorBt2ShutterController(StandardShutterPluginController, HardwarePluginController):
+class UrsaMinorBt2ShutterController(ShutterPluginController, HardwarePluginController):
+    def _valueChanged(self, value=None):
+        ShutterPluginController._valueChanged(self, value)
+        self.refreshView()
+
     def _defineGui(self):
         Logger().trace("UrsaMinorBt2ShutterController._defineGui()")
-        StandardShutterPluginController._defineGui(self)
+        ShutterPluginController._defineGui(self)
         HardwarePluginController._defineGui(self)
         self._addTab('Focus', TAB_FOCUS)
         self._addWidget('Focus', LABEL_FOCUS_ENABLE, CheckBoxField, (), 'FOCUS_ENABLE')
-        self._addWidget('Focus', LABEL_PULSE_WIDTH, DoubleSpinBoxField, (0.1, 5., 1, 0.1, "", " s"), 'FOCUS_PULSE_WIDTH')
+        self._addWidget('Focus', LABEL_FOCUS_PULSE_WIDTH, DoubleSpinBoxField, (0.1, 5., 1, 0.1, "", " s"), 'FOCUS_PULSE_WIDTH')
         self._addWidget('Focus', LABEL_MAINTAIN_FOCUS, CheckBoxField, (), 'FOCUS_MAINTAIN')
+
+    def refreshView(self):
+        focus = self._getWidget('Focus', LABEL_FOCUS_ENABLE).value()
+        self._getWidget('Focus', LABEL_FOCUS_PULSE_WIDTH).setDisabled(not focus)
+        self._getWidget('Focus', LABEL_MAINTAIN_FOCUS).setDisabled(not focus)
 
 
 def register():
