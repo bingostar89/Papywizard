415ae1e010d096f753a7f0103c58bc79a6317582
fma <fma@1bb8f0fc-47fd-4586-b4e1-c5ba9e517023>
2011-02-04 10:26:13 +0000

    Changed path to view

--- papywizard/controller/abstractController.py
+++ papywizard/controller/abstractController.py
@@ -9,7 +9,7 @@ License
   - (C) 2007-2010 Frédéric Mantegazza
 
 This software is governed by the B{CeCILL} license under French law and
-abiding by the rules of distribution of free software.  You can  use, 
+abiding by the rules of distribution of free software.  You can  use,
 modify and/or redistribute the software under the terms of the CeCILL
 license as circulated by CEA, CNRS and INRIA at the following URL
 U{http://www.cecill.info}.
@@ -18,7 +18,7 @@ As a counterpart to the access to the source code and  rights to copy,
 modify and redistribute granted by the license, users are provided only
 with a limited warranty  and the software's author,  the holder of the
 economic rights,  and the successive licensors  have only  limited
-liability. 
+liability.
 
 In this respect, the user's attention is drawn to the risks associated
 with loading,  using,  modifying and/or developing or reproducing the
@@ -27,9 +27,9 @@ that may mean  that it is complicated to manipulate,  and  that  also
 therefore means  that it is reserved for developers  and  experienced
 professionals having in-depth computer knowledge. Users are therefore
 encouraged to load and test the software's suitability as regards their
-requirements in conditions enabling the security of their systems and/or 
-data to be ensured and,  more generally, to use and operate it in the 
-same conditions as regards security. 
+requirements in conditions enabling the security of their systems and/or
+data to be ensured and,  more generally, to use and operate it in the
+same conditions as regards security.
 
 The fact that you are presently reading this means that you have had
 knowledge of the CeCILL license and that you accept its terms.
@@ -61,7 +61,7 @@ from PyQt4 import QtCore, QtGui
 from papywizard.common.loggingServices import Logger
 
 if hasattr(sys, "frozen"):
-    path = os.path.join(os.path.dirname(os.path.abspath(sys.argv[0])), "papywizard", "controller")
+    path = os.path.join(os.path.dirname(os.path.abspath(sys.argv[0])), "papywizard", "view")
 else:
     path = os.path.dirname(__file__)
 
@@ -86,7 +86,7 @@ class AbstractController(QtCore.QObject):
         self._init()
 
         # Build the GUI from ui file
-        uiFile = os.path.join(path, os.path.pardir, "view", "ui", self._uiFile)
+        uiFile = os.path.join(path, "ui", self._uiFile)
         self._view = PyQt4.uic.loadUi(uiFile)
 
         self._initWidgets()
